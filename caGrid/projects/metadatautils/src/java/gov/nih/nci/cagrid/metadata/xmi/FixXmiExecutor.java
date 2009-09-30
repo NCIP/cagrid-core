4d502061c4f81483c3b6bf3aee42c8846b6f4964
osters <osters>
2009-09-30 19:49:18 +0000

    i "think" this fixes the spaces issue
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@14508 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/metadatautils/src/java/gov/nih/nci/cagrid/metadata/xmi/FixXmiExecutor.java
+++ caGrid/projects/metadatautils/src/java/gov/nih/nci/cagrid/metadata/xmi/FixXmiExecutor.java
@@ -13,50 +13,48 @@ import java.util.StringTokenizer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-/** 
- *  FixXmiExecutor
- *  Executes SDK 3.1 / 3.2 / 3.2.1's fix-xmi ant target
+
+/**
+ * FixXmiExecutor Executes SDK 3.1 / 3.2 / 3.2.1's fix-xmi ant target
  * 
  * @author David Ervin
- * 
  * @created Oct 29, 2007 2:11:31 PM
- * @version $Id: FixXmiExecutor.java,v 1.9 2008-04-28 19:31:07 dervin Exp $ 
+ * @version $Id: FixXmiExecutor.java,v 1.9 2008-04-28 19:31:07 dervin Exp $
  */
 public class FixXmiExecutor {
     public static final Log LOG = LogFactory.getLog(FixXmiExecutor.class);
-    
+
     // ant tasks
     public static final String FIX_XMI_TASK = "fix-xmi";
     public static final String COMPILE_GENERATOR_TASK = "compile-generator";
-    
+
     // properties for the ant tasks
     public static final String MODEL_DIR_PROPERTY = "dir.model";
     public static final String MODEL_FILENAME_PROPERTY = "model_filename";
     public static final String FIXED_MODEL_FILENAME_PROPERTY = "fixed_filename";
     public static final String PREPROCESSOR_PROPERTY = "xmi_preprocessor";
-    
+
     // the EA xmi preprocessor class
-    public static final String EA_XMI_PREPROCESSOR = 
-        "gov.nih.nci.codegen.core.util.EAXMIPreprocessor";
-    
+    public static final String EA_XMI_PREPROCESSOR = "gov.nih.nci.codegen.core.util.EAXMIPreprocessor";
+
     private static Boolean isWindows = null;
-    
+
+
     private FixXmiExecutor() {
         // prevent instantiation
     }
-    
-    
+
+
     /**
      * Runs the SDK's fix-xmi target against the specified model
+     * 
      * @param originalModel
-     *      The file containing the original XMI model from EA
+     *            The file containing the original XMI model from EA
      * @param sdkDir
-     *      The caCORE SDK base directory
-     * @return
-     *      The file containing the 'fixed' model
+     *            The caCORE SDK base directory
+     * @return The file containing the 'fixed' model
      */
-    public static File fixEaXmiModel(File originalModel, File sdkDir) throws IOException,
-        InterruptedException {
+    public static File fixEaXmiModel(File originalModel, File sdkDir) throws IOException, InterruptedException {
         File cleanModelFile = cleanXmi(originalModel);
         List<String> command = new ArrayList<String>();
         // get the base ant command
@@ -64,17 +62,15 @@ public class FixXmiExecutor {
         // add properties and their values
         String modelDirProp = "-D" + MODEL_DIR_PROPERTY + "=";
         String modelFileDir = cleanModelFile.getAbsoluteFile().getParent();
-        if (osIsWindows()) {
-            modelDirProp += modelFileDir;
-        } else {
-            // escape spaces
-            modelDirProp += modelFileDir.replace(" ", "\\ ");
-        }
+
+        modelDirProp += modelFileDir;
+
         command.add(modelDirProp);
         command.add("-D" + MODEL_FILENAME_PROPERTY + "=" + cleanModelFile.getName());
         command.add("-D" + FIXED_MODEL_FILENAME_PROPERTY + "=fixed_" + originalModel.getName());
         command.add("-D" + PREPROCESSOR_PROPERTY + "=" + EA_XMI_PREPROCESSOR);
-        // windows command line issues: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6468220
+        // windows command line issues:
+        // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6468220
         String[] cmdArray = command.toArray(new String[0]);
         if (osIsWindows()) {
             for (int i = 0; i < cmdArray.length; i++) {
@@ -83,19 +79,19 @@ public class FixXmiExecutor {
                 }
             }
         }
-        //System.out.println("Executing command...");
-        LOG.debug("Executing command...");
-        for (String c : cmdArray) {
-            LOG.debug("\t" + c);
-            //System.out.println("\t" + c);
+        // System.out.println("Executing command...");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Executing command...");
+            for (String c : cmdArray) {
+                LOG.debug("\t" + c);
+                // System.out.println("\t" + c);
+            }
         }
         // execute the command
         Process proc = Runtime.getRuntime().exec(cmdArray);
         // streams to LOG
-        new StreamGobbler(proc.getInputStream(), StreamGobbler.TYPE_OUT,
-            LOG, LogPriority.DEBUG).start();
-        new StreamGobbler(proc.getErrorStream(), StreamGobbler.TYPE_ERR,
-            LOG, LogPriority.DEBUG).start();
+        new StreamGobbler(proc.getInputStream(), StreamGobbler.TYPE_OUT, LOG, LogPriority.DEBUG).start();
+        new StreamGobbler(proc.getErrorStream(), StreamGobbler.TYPE_ERR, LOG, LogPriority.DEBUG).start();
         LOG.debug("Waiting");
         proc.waitFor();
         if (proc.exitValue() == 0) {
@@ -104,8 +100,8 @@ public class FixXmiExecutor {
             throw new RuntimeException("Error executing fix-xmi command:\n" + command.toString());
         }
     }
-    
-    
+
+
     private static List<String> getAntCall(String buildFileDir) {
         List<String> command = new ArrayList<String>();
         if (osIsWindows()) {
@@ -117,10 +113,9 @@ public class FixXmiExecutor {
             command.add(buildFileDir + File.separator + "build.xml");
         } else {
             // escape out the spaces.....
-            buildFileDir = buildFileDir.replace(" ", "\\ ");
             command.add("java");
             command.add("-classpath");
-            command.add(getAntLauncherJarLocation(System.getProperty("java.class.path").replace(" ", "\\ ")));
+            command.add(getAntLauncherJarLocation(System.getProperty("java.class.path")));
             command.add("org.apache.tools.ant.launch.Launcher");
             command.add("-buildfile");
             command.add(buildFileDir + File.separator + "build.xml");
@@ -129,8 +124,8 @@ public class FixXmiExecutor {
         command.add(FIX_XMI_TASK);
         return command;
     }
-    
-    
+
+
     private static String getAntLauncherJarLocation(String path) {
         StringTokenizer pathTokenizer = new StringTokenizer(path, File.pathSeparator);
         while (pathTokenizer.hasMoreTokens()) {
@@ -141,8 +136,8 @@ public class FixXmiExecutor {
         }
         return null;
     }
-    
-    
+
+
     private static File cleanXmi(File originalXmi) throws IOException {
         LOG.debug("Clean XMI");
         File cleanedFile = new File(originalXmi.getParentFile(), "cleaned_" + originalXmi.getName());
@@ -151,8 +146,8 @@ public class FixXmiExecutor {
         Utils.stringBufferToFile(xmiContents, cleanedFile.getAbsolutePath());
         return cleanedFile;
     }
-    
-    
+
+
     private static boolean osIsWindows() {
         if (isWindows == null) {
             String os = System.getProperty("os.name").toLowerCase();
@@ -160,7 +155,7 @@ public class FixXmiExecutor {
         }
         return isWindows.booleanValue();
     }
-    
+
 
     static boolean needsQuoting(String s) {
         int len = s.length();
@@ -168,15 +163,19 @@ public class FixXmiExecutor {
             return true;
         for (int i = 0; i < len; i++) {
             switch (s.charAt(i)) {
-                case ' ': case '\t': case '\\': case '"':
+                case ' ' :
+                case '\t' :
+                case '\\' :
+                case '"' :
                     return true;
             }
         }
         return false;
     }
 
+
     static String winQuote(String s) {
-        if (! needsQuoting(s))
+        if (!needsQuoting(s))
             return s;
         s = s.replaceAll("([\\\\]*)\"", "$1$1\\\\\"");
         s = s.replaceAll("([\\\\]*)\\z", "$1$1");
