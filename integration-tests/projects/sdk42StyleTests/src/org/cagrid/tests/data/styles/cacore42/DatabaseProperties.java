df2d811b89ff799a8f34ef2aae4e974d5d7b6954
ervind <ervind>
2009-10-14 19:40:35 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@14743 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ integration-tests/projects/sdk42StyleTests/src/org/cagrid/tests/data/styles/cacore42/DatabaseProperties.java
@@ -0,0 +1,58 @@
+package org.cagrid.tests.data.styles.cacore42;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Reads database properties
+ * 
+ * @author David
+ */
+public class DatabaseProperties {
+    
+    public static final String PROPERTIES_FILENAME = "sdk42.test.database.properties";
+    
+    private static Properties props = null;
+    
+    private DatabaseProperties() {
+        // prevents instantiating this thing
+    }
+    
+    
+    private static synchronized Properties getProps() throws IOException {
+        if (props == null) {
+            props = new Properties();
+            InputStream propsIn = new FileInputStream(PROPERTIES_FILENAME);
+            props.load(propsIn);
+            propsIn.close();
+        }
+        return props;
+    }
+    
+    
+    public static String getServer() throws IOException {
+        return getProps().getProperty("db.server");
+    }
+    
+    
+    public static String getPort() throws IOException {
+        return getProps().getProperty("db.port");
+    }
+    
+    
+    public static String getSchemaName() throws IOException {
+        return getProps().getProperty("db.schema.name");
+    }
+    
+    
+    public static String getUsername() throws IOException {
+        return getProps().getProperty("db.username");
+    }
+    
+    
+    public static String getPassword() throws IOException {
+        return getProps().getProperty("db.password");
+    }
+}
