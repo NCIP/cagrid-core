18c6b9a8e81d049778dcc1a81e13611aab718e2b
ervind <ervind>
2009-10-19 16:14:28 +0000

    util to edit properties files and preserve comments and formatting
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@14799 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/PropertiesPreservingComments.java
@@ -0,0 +1,194 @@
+package gov.nih.nci.cagrid.common;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/** 
+ *  PropertiesPreservingComments
+ *  Properties which maintains comment lines, blank lines, and ordering when stored
+ * 
+ * @author David Ervin
+ * 
+ * @created Jun 15, 2007 11:28:40 AM
+ * @version $Id: PropertiesPreservingComments.java,v 1.2 2007-06-18 14:20:46 dervin Exp $ 
+ */
+public class PropertiesPreservingComments {
+    
+    private Map<String, Integer> propertyIndices = null;
+    private List<String> lines = null;
+    
+    public PropertiesPreservingComments() {
+        propertyIndices = new HashMap<String, Integer>();
+        lines = new ArrayList<String>();
+    }
+    
+    
+    public void load(File file) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        load(fis);
+        fis.close();
+    }
+    
+    
+    public void load(InputStream stream) throws IOException {
+        StringBuffer rawProps = Utils.inputStreamToStringBuffer(stream);
+        BufferedReader reader = new BufferedReader(new StringReader(rawProps.toString()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            if (!"\n".equals(line)) {
+                line = line + "\n";
+            }
+            lines.add(line);
+            if (!line.startsWith("#")) {
+                String key = getPropertyKeyFromLine(line);
+                if (key != null) {
+                    propertyIndices.put(key, Integer.valueOf(lines.size() - 1));
+                }
+            }
+        }
+    }
+    
+    
+    public void write(OutputStream stream) {
+        PrintWriter writer = new PrintWriter(stream);
+        for (String line : lines) {
+            writer.print(line);
+        }
+        writer.flush();
+    }
+    
+    
+    public String getProperty(String key) {
+        return getProperty(key, null);
+    }
+    
+    
+    public String getProperty(String key, String defaultValue) {
+        Integer lineIndex = propertyIndices.get(key);
+        if (lineIndex == null) {
+            return defaultValue;
+        }
+        String line = lines.get(lineIndex.intValue());
+        return getPropertyValueFromLine(line);
+    }
+    
+    
+    public Object setProperty(String key, String value) {
+        // if the property already exists but is just commented out, uncomment it
+        uncommentProperty(key);
+        String line = key + "=" + value + "\n";
+        String oldValue = null;
+        // see if the property exists
+        Integer lineIndex = propertyIndices.get(key);
+        if (lineIndex != null) {
+            oldValue = getProperty(key);
+            lines.set(lineIndex.intValue(), line);
+        } else {
+            lines.add(line);
+            propertyIndices.put(key, Integer.valueOf(lines.size() - 1));
+        }
+        return oldValue;
+    }
+    
+    
+    public Object remove(Object key) {
+        // if the property exists but is commented, uncomment it so it can be found
+        uncommentProperty((String) key);
+        Object oldValue = getProperty((String) key);
+        Integer lineIndex = propertyIndices.remove(key);
+        if (lineIndex != null) {
+            lines.remove(lineIndex.intValue());
+        }
+        return oldValue;
+    }
+    
+    
+    /**
+     * Comments out the property.
+     * 
+     * @param key
+     *      The key of the property to comment out
+     * @return
+     *      True if the property existed and was commented out, false otherwise
+     */
+    public boolean commentOutProperty(String key) {
+        boolean commented = false;
+        if (propertyIndices.containsKey(key)) {
+            Integer lineNum = propertyIndices.remove(key);
+            String line = "#" + lines.get(lineNum.intValue());
+            lines.set(lineNum.intValue(), line);
+            commented = true;
+        }
+        return commented;
+    }
+    
+    
+    /**
+     * Uncomments a property
+     * 
+     * @param key
+     *      The key of the property to uncomment
+     * @return
+     *      True if the property existed and was uncommented
+     */
+    public boolean uncommentProperty(String key) throws IllegalStateException {
+        // since the key is no longer in the properties index, we have to search for it
+        int lineNum = 0;
+        int foundIndex = -1;
+        for (; lineNum < lines.size(); lineNum++) {
+            String line = lines.get(lineNum);
+            String uncommentedLine = trimLeadingHashes(line);
+            String lineKey = getPropertyKeyFromLine(uncommentedLine);
+            boolean found = lineKey != null && lineKey.equals(key);
+            if (found) {
+                if (foundIndex != -1) {
+                    throw new IllegalStateException("A property with key " + key + " was found multiple times!");
+                } else {
+                    foundIndex = lineNum;
+                }
+            }
+        }
+        if (foundIndex != -1) {
+            lines.set(foundIndex, trimLeadingHashes(lines.get(foundIndex)));
+            propertyIndices.put(key, Integer.valueOf(foundIndex));
+        }
+        return foundIndex != -1;
+    }
+    
+    
+    private String getPropertyKeyFromLine(String line) {
+        int split = line.indexOf('=');
+        return split != -1 ? line.substring(0, split) : null;
+    }
+    
+    
+    private String getPropertyValueFromLine(String line) {
+        int split = line.indexOf('=');
+        if (line.length() > split) {
+            return line.substring(split + 1, line.length() - 1);
+        }
+        return "";
+    }
+    
+    
+    private String trimLeadingHashes(String line) {
+        int i = 0;
+        for (; i < line.length(); i++) {
+            if (line.charAt(i) != '#') {
+                break;
+            }
+        }
+        return line.substring(i);
+    }
+}
---
+++ caGrid/projects/core/test/src/java/gov/nih/nci/cagrid/common/PropertiesPreservingCommentsTestCase.java
@@ -0,0 +1,176 @@
+package gov.nih.nci.cagrid.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+
+public class PropertiesPreservingCommentsTestCase extends TestCase {
+    
+    private static final String RAW_PROPERTIES = 
+        "\n" +
+        "property1=value1\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    private static final String SET_PROPERTY1 = 
+        "\n" +
+        "property1=changed\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    private static final String SET_PROPERTY4 = 
+        "\n" +
+        "property1=value1\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=changed\n";
+    
+    private static final String ADD_PROPERTY5 = 
+        "\n" +
+        "property1=value1\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n" +
+        "property5=value5\n";
+    
+    private static final String REMOVE_PROPERTY2 = 
+        "\n" +
+        "property1=value1\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    private static final String COMMENT_PROPERTY2 = 
+        "\n" +
+        "property1=value1\n" +
+        "#property2=value2\n" +
+        "\n" +
+        "\n" +
+        "#property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    private static final String UNCOMMENT_PROPERTY3 = 
+        "\n" +
+        "property1=value1\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "property3=value3\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    private static final String SET_PROPERTY3 = 
+        "\n" +
+        "property1=value1\n" +
+        "property2=value2\n" +
+        "\n" +
+        "\n" +
+        "property3=changed\n" +
+        "### some comment\n" +
+        "\n" +
+        "property4=value4\n";
+    
+    
+    private PropertiesPreservingComments properties = null;
+
+    public PropertiesPreservingCommentsTestCase(String name) {
+        super(name);
+    }
+    
+    
+    public void setUp() {
+        properties = new PropertiesPreservingComments();
+        InputStream propertiesIn = new ByteArrayInputStream(RAW_PROPERTIES.getBytes());
+        try {
+            properties.load(propertiesIn);
+            propertiesIn.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            fail("Error loading properties: " + ex.getMessage());
+        }
+    }
+    
+    
+    private void compareProperties(String gold) {
+        ByteArrayOutputStream propsOut = new ByteArrayOutputStream();
+        properties.write(propsOut);
+        String propertiesString = propsOut.toString();
+        assertEquals("Edited properties differed from expected", gold, propertiesString);
+    }
+    
+    
+    public void testSetProperty1() {
+        Object oldValue = properties.setProperty("property1", "changed");
+        assertEquals("Old value of property1 was not expected", "value1", oldValue);
+        compareProperties(SET_PROPERTY1);
+    }
+    
+    
+    public void testSetProperty4() {
+        Object oldValue = properties.setProperty("property4", "changed");
+        assertEquals("Old value of property4 was not expected", "value4", oldValue);
+        compareProperties(SET_PROPERTY4);
+    }
+    
+    
+    public void testAddProperty5() {
+        properties.setProperty("property5", "value5");
+        compareProperties(ADD_PROPERTY5);
+    }
+    
+    
+    public void testRemoveProperty2() {
+        properties.remove("property2");
+        compareProperties(REMOVE_PROPERTY2);
+    }
+    
+    
+    public void testCommentProperty2() {
+        boolean commented = properties.commentOutProperty("property2");
+        assertTrue("Property was apparently not commented out", commented);
+        compareProperties(COMMENT_PROPERTY2);
+    }
+    
+    
+    public void testUncommentProperty3() {
+        boolean uncommented = properties.uncommentProperty("property3");
+        assertTrue("Property was apparently not uncommented", uncommented);
+        String value = properties.getProperty("property3");
+        assertEquals("Value of property3 was not expected", "value3", value);
+        compareProperties(UNCOMMENT_PROPERTY3);
+    }
+    
+    
+    public void testSetProperty3() {
+        Object oldValue = properties.setProperty("property3", "changed");
+        assertEquals("Old value of property3 was not expected", "value3", oldValue);
+        compareProperties(SET_PROPERTY3);
+    }
+}
