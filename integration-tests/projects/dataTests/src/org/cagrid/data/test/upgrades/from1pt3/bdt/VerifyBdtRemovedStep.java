7a3e5472e1f006d49783037307bdb1a0de1cdeed
ervind <ervind>
2009-10-23 14:25:31 +0000

    upgrade tests for BDT and wsEnum
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@14840 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ integration-tests/projects/dataTests/src/org/cagrid/data/test/upgrades/from1pt3/bdt/VerifyBdtRemovedStep.java
@@ -0,0 +1,31 @@
+package org.cagrid.data.test.upgrades.from1pt3.bdt;
+
+import gov.nih.nci.cagrid.data.DataServiceConstants;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodType;
+import gov.nih.nci.cagrid.introduce.beans.service.ServiceType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
+import gov.nih.nci.cagrid.testing.system.haste.Step;
+
+import java.io.File;
+
+import org.cagrid.data.test.creation.DataTestCaseInfo;
+
+public class VerifyBdtRemovedStep extends Step {
+    
+    private DataTestCaseInfo testInfo = null;
+    
+    public VerifyBdtRemovedStep(DataTestCaseInfo testInfo) {
+        super();
+        this.testInfo = testInfo;
+    }
+    
+
+    public void runStep() throws Throwable {
+        ServiceInformation info = new ServiceInformation(new File(testInfo.getDir()));
+        ServiceType baseService = info.getServices().getService(0);
+        MethodType bdtQueryMethod = CommonTools.getMethod(baseService.getMethods(), 
+            DataServiceConstants.BDT_QUERY_METHOD_NAME);
+        assertNull(DataServiceConstants.BDT_QUERY_METHOD_NAME + " was not removed from the service", bdtQueryMethod);
+    }
+}
---
+++ integration-tests/projects/dataTests/src/org/cagrid/data/test/upgrades/from1pt3/enumeration/UpgradeEnumerationFrom1pt3Tests.java
@@ -0,0 +1,95 @@
+package org.cagrid.data.test.upgrades.from1pt3.enumeration;
+
+import gov.nih.nci.cagrid.testing.system.haste.Step;
+import gov.nih.nci.cagrid.testing.system.haste.Story;
+
+import java.util.Vector;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import org.cagrid.data.test.creation.DataTestCaseInfo;
+import org.cagrid.data.test.creation.DeleteOldServiceStep;
+import org.cagrid.data.test.upgrades.BuildUpgradedServiceStep;
+import org.cagrid.data.test.upgrades.UnpackOldServiceStep;
+import org.cagrid.data.test.upgrades.UpgradeIntroduceServiceStep;
+import org.cagrid.data.test.upgrades.UpgradeTestConstants;
+
+/** 
+ *  UpgradeEnumerationFrom1pt3Tests
+ *  Tests to upgrade an enumeration data service from 1.3 to current
+ * 
+ * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>  * 
+ * @created Feb 20, 2007 
+ * @version $Id: UpgradeEnumerationFrom1pt3Tests.java,v 1.1 2008-10-28 15:55:16 dervin Exp $ 
+ */
+public class UpgradeEnumerationFrom1pt3Tests extends Story {
+    public static final String SERVICE_ZIP_NAME = "DataServiceWithEnumeration_1-3.zip";
+    public static final String SERVICE_DIR_NAME = "DataServiceWithEnumeration_1-3";
+    public static final String SERVICE_NAME = "DataServiceWithEnum";
+    public static final String SERVICE_PACKAGE = "org.cagrid.test.data.with.wsenum";
+    public static final String SERVICE_NAMESPACE = "http://wsenum.with.data.test.cagrid.org/DataServiceWithEnum";
+    
+    private DataTestCaseInfo testServiceInfo = null;
+	
+	public String getDescription() {
+		return "Tests upgrade of an enumeration data service from version 1.3 to " + UpgradeTestConstants.getCurrentDataVersion();
+	}
+    
+    
+    public String getName() {
+        return "Data Service With Enumeration 1_3 to " 
+            + UpgradeTestConstants.getCurrentDataVersion().replace(".", "_") 
+            + " Upgrade Tests";
+    }
+    
+    
+    public boolean storySetUp() {
+        this.testServiceInfo = new DataTestCaseInfo() {
+            public String getServiceDirName() {
+                return SERVICE_DIR_NAME;
+            }
+
+
+            public String getName() {
+                return SERVICE_NAME;
+            }
+
+
+            public String getNamespace() {
+                return SERVICE_NAMESPACE;
+            }
+
+
+            public String getPackageName() {
+                return SERVICE_PACKAGE;
+            }
+        };
+        return true;
+    }
+	
+
+	protected Vector steps() {
+        Vector<Step> steps = new Vector<Step>();
+		// steps to unpack and upgrade the old service
+		steps.add(new DeleteOldServiceStep(testServiceInfo));
+		steps.add(new UnpackOldServiceStep(SERVICE_ZIP_NAME));
+		steps.add(new UpgradeIntroduceServiceStep(testServiceInfo.getDir()));
+		steps.add(new BuildUpgradedServiceStep(testServiceInfo.getDir()));		
+		return steps;
+	}
+    
+    
+    protected void storyTearDown() throws Throwable {
+        Step deleteServiceStep = new DeleteOldServiceStep(testServiceInfo);
+        deleteServiceStep.runStep();
+    }
+
+
+	public static void main(String[] args) {
+		TestRunner runner = new TestRunner();
+		TestResult result = runner.doRun(new TestSuite(UpgradeEnumerationFrom1pt3Tests.class));
+		System.exit(result.errorCount() + result.failureCount());
+	}
+}
