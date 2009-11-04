088611e554d014d4387894ea36a2d63c41da3d7e
hastingss2 <hastingss2>
2009-11-04 18:31:34 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15010 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/transfer/src/org/cagrid/transfer/context/service/globus/resource/PersistentTransferCallback.java
+++ caGrid/projects/transfer/src/org/cagrid/transfer/context/service/globus/resource/PersistentTransferCallback.java
@@ -3,6 +3,13 @@ package org.cagrid.transfer.context.service.globus.resource;
 import org.cagrid.transfer.context.service.helper.DataStagedCallback;
 import org.cagrid.transfer.context.stubs.types.TransferServiceContextReference;
 
+/*
+ * PersistentTransferCallback should be implemented by an Resource using the TransferServiceHelper
+ * to create upload transfers with callbacks.  This interface will enable the TransferServiceContextResource 
+ * to call back into your resource to get a handle to the DataStorageCallback if the TransferServiceContextResource
+ * gets garbage collected or the container restarts.  Implemntation of this interface on the Resource of your service
+ * which is using Transfers for upload is required to avoid bug CAGRID-100.
+ */
 public interface PersistentTransferCallback {
 	
 	public DataStagedCallback getCallback(TransferServiceContextReference ref) throws Exception;
