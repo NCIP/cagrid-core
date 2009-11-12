007436b4cfbbd31cb270d1fe8d8bcfea415c40b2
osters <osters>
2009-11-12 20:54:37 +0000

    typo
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15066 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/modification/extensions/ExtensionsManagerPanel.java
+++ caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/modification/extensions/ExtensionsManagerPanel.java
@@ -219,7 +219,7 @@ public class ExtensionsManagerPanel extends JPanel {
                             IntroduceConstants.INTRODUCE_SKELETON_EXTENSIONS, extS);
                         JOptionPane
                             .showMessageDialog(GridApplication.getContext().getApplication(),
-                                "After adding a new extension a save should be perfomred in order for the extension to be active.");
+                                "After adding a new extension a save should be performed in order for the extension to be active.");
                     }
                 }
             });
