bca2b981d23faa2093da20ef99c22ae5e8c264b0
ervind <ervind>
2009-11-19 19:05:20 +0000

    CAGRID-89: moving ivy cache nuke out to a story so it can be run once
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15124 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ integration-tests/projects/sdk42StyleTests/src/org/cagrid/tests/data/styles/cacore42/story/NukeIvyCacheStory.java
@@ -0,0 +1,33 @@
+package org.cagrid.tests.data.styles.cacore42.story;
+
+import java.util.Vector;
+
+import org.cagrid.tests.data.styles.cacore42.steps.NukeIvyCacheStep;
+
+import gov.nih.nci.cagrid.testing.system.haste.Step;
+import gov.nih.nci.cagrid.testing.system.haste.Story;
+
+public class NukeIvyCacheStory extends Story {
+
+    public NukeIvyCacheStory() {
+        super();
+    }
+
+
+    public String getDescription() {
+        return "Deletes the Ivy cache used by the caCORE SDK build process";
+    }
+    
+    
+    public String getName() {
+        return "Nuke Ivy Cache Story";
+    }
+
+
+    protected Vector steps() {
+        Vector<Step> steps = new Vector<Step>();
+        steps.add(new NukeIvyCacheStep());
+        return steps;
+    }
+
+}
