69510b15fe8ba9dcb3caa4685f268b4442e39436
ervind <ervind>
2009-12-21 16:52:11 +0000

    CAGRID-234: Enum to identify which part of CQL 2 is being extended
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15379 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/data/src/java/cql/gov/nih/nci/cagrid/data/cql2/Cql2ExtensionPoint.java
@@ -0,0 +1,11 @@
+package gov.nih.nci.cagrid.data.cql2;
+
+/**
+ * Identifies the part of a CQL 2 query being extended
+ * 
+ * @author David
+ */
+public enum Cql2ExtensionPoint {
+
+    OBJECT, ATTRIBUTE, MODIFIER, RESULT;
+}
