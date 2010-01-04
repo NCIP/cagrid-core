8cef85d78746b7d5589d75adc10c39c64a8b291a
ervind <ervind>
2010-01-04 21:32:17 +0000

    spelling
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15416 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/transfer/service/globus/TransferDataServiceProviderImpl.java
+++ caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/transfer/service/globus/TransferDataServiceProviderImpl.java
@@ -10,8 +10,8 @@ import java.rmi.RemoteException;
  *
  * This class implements each method in the portType of the service.  Each method call represented
  * in the port type will be then mapped into the unwrapped implementation which the user provides
- * in the EnumerationDataServiceImpl class.  This class handles the boxing and unboxing of each method call
- * so that it can be correclty mapped in the unboxed interface that the developer has designed and 
+ * in the TransferDataServiceImpl class.  This class handles the boxing and unboxing of each method call
+ * so that it can be correctly mapped in the unboxed interface that the developer has designed and 
  * has implemented.  Authorization callbacks are automatically made for each method based
  * on each methods authorization requirements.
  * 
