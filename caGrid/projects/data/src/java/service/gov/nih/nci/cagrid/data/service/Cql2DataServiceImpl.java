f633bda278473abaff9d481fc9c60c5640124f76
ervind <ervind>
2010-01-05 20:23:11 +0000

    CAGRID-232: Basic CQL2 query method implementation and provider
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15430 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/service/Cql2DataServiceImpl.java
@@ -0,0 +1,29 @@
+package gov.nih.nci.cagrid.data.service;
+
+import gov.nih.nci.cagrid.data.MalformedQueryException;
+import gov.nih.nci.cagrid.data.QueryProcessingException;
+import gov.nih.nci.cagrid.data.faults.MalformedQueryExceptionType;
+import gov.nih.nci.cagrid.data.faults.QueryProcessingExceptionType;
+
+import org.cagrid.cql2.CQLQuery;
+import org.cagrid.cql2.results.CQLQueryResults;
+
+public class Cql2DataServiceImpl extends BaseDataServiceImpl {
+
+    public Cql2DataServiceImpl() throws DataServiceInitializationException {
+        super();
+    }
+    
+    
+    public CQLQueryResults executeQuery(CQLQuery query) throws QueryProcessingExceptionType, MalformedQueryExceptionType {
+        CQLQueryResults results;
+        try {
+            results = processCql2Query(query);
+        } catch (QueryProcessingException ex) {
+            throw getTypedException(ex, new QueryProcessingExceptionType());
+        } catch (MalformedQueryException ex) {
+            throw getTypedException(ex, new MalformedQueryExceptionType());
+        }
+        return results;
+    }
+}
