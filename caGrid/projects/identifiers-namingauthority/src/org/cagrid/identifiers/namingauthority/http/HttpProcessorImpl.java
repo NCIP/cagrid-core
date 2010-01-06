cbb9f36cca66fe2b6cf8d5bc70be38a7fe27e919
cmelean <cmelean>
2010-01-06 19:22:44 +0000

    removed unmappable characters for encoding UTF8
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15446 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/identifiers-namingauthority/src/org/cagrid/identifiers/namingauthority/http/HttpProcessorImpl.java
+++ caGrid/projects/identifiers-namingauthority/src/org/cagrid/identifiers/namingauthority/http/HttpProcessorImpl.java
@@ -158,23 +158,23 @@ public class HttpProcessorImpl implements HttpProcessor {
 //        with a custom www-authenticate method. 
 //        403 Substatus Error Codes for IIS 
 //
-//            * 403.1 - Execute access forbidden. 
-//            * 403.2 - Read access forbidden. 
-//            * 403.3 - Write access forbidden. 
-//            * 403.4 - SSL required. 
-//            * 403.5 - SSL 128 required. 
-//            * 403.6 - IP address rejected. 
-//            * 403.7 - Client certificate required. 
-//            * 403.8 - Site access denied. 
-//            * 403.9 - Too many users. 
-//            * 403.10 - Invalid configuration. 
-//            * 403.11 - Password change. 
-//            * 403.12 - Mapper denied access. 
-//            * 403.13 - Client certificate revoked. 
-//            * 403.14 - Directory listing denied. 
-//            * 403.15 - Client Access Licenses exceeded. 
-//            * 403.16 - Client certificate is untrusted or invalid. 
-//            * 403.17 - Client certificate has expired or is not yet valid. 
+//        * 403.1 - Execute access forbidden. 
+//        * 403.2 - Read access forbidden. 
+//        * 403.3 - Write access forbidden. 
+//        * 403.4 - SSL required. 
+//        * 403.5 - SSL 128 required. 
+//        * 403.6 - IP address rejected. 
+//        * 403.7 - Client certificate required. 
+//        * 403.8 - Site access denied. 
+//        * 403.9 - Too many users. 
+//        * 403.10 - Invalid configuration. 
+//        * 403.11 - Password change. 
+//        * 403.12 - Mapper denied access. 
+//        * 403.13 - Client certificate revoked. 
+//        * 403.14 - Directory listing denied. 
+//        * 403.15 - Client Access Licenses exceeded. 
+//        * 403.16 - Client certificate is untrusted or invalid. 
+//        * 403.17 - Client certificate has expired or is not yet valid. 
 
         //
         // ?config causes to ignore resolution and
