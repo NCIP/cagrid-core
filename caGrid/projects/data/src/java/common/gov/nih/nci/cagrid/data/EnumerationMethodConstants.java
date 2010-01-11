09a5a00718558939252dd950cda9734e1f4796de
ervind <ervind>
2010-01-11 18:48:50 +0000

    CAGRID-224: Fixing up namespaces
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15486 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/EnumerationMethodConstants.java
+++ caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/EnumerationMethodConstants.java
@@ -4,7 +4,7 @@ import javax.xml.namespace.QName;
 
 public interface EnumerationMethodConstants {
 
-    // enumeration query method constants
+    // CQL 1 enumeration query method constants
     public static final String ENUMERATION_QUERY_METHOD_DESCRIPTION = "The standard caGrid Data Service query method which begins an Enumeration";
     public static final String ENUMERATION_QUERY_METHOD_NAME = "enumerationQuery";
     public static final String ENUMERATION_DATA_SERVICE_PACKAGE = ServiceNamingConstants.DATA_SERVICE_PACKAGE + ".enumeration";
@@ -13,4 +13,9 @@ public interface EnumerationMethodConstants {
     public static final String ENUMERATION_RESPONSE_NAMESPACE = "http://gov.nih.nci.cagrid.enumeration/EnumerationResponseContainer";
     public static final QName ENUMERATION_QUERY_METHOD_OUTPUT_TYPE = new QName(ENUMERATION_RESPONSE_NAMESPACE, "EnumerationResponseContainer");
     
+    // CQL 2 enumeration query method constants
+    public static final String CQL2_ENUMERATION_QUERY_METHOD_DESCRIPTION = "The standard caGrid Data Service query method which takes CQL 2 and begins an Enumeration";
+    public static final String CQL2_ENUMERATION_QUERY_METHOD_NAME = "executeEnumerationQuery";
+    public static final String CQL2_ENUMERATION_DATA_SERVICE_PACKAGE = ServiceNamingConstants.CQL2_DATA_SERVICE_PACKAGE + ".enumeration";
+    public static final String CQL2_ENUMERATION_DATA_SERVICE_NAMESPACE = "http://org.cagrid.dataservice.enumeration/Cql2EnumerationDataService";   
 }
