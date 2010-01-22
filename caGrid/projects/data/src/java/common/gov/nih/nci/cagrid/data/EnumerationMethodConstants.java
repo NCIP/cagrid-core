cbe3e4e75bb5cb1dee204f0eb07181a37129afd7
ervind <ervind>
2010-01-22 14:04:06 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15565 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/EnumerationMethodConstants.java
+++ caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/EnumerationMethodConstants.java
@@ -12,10 +12,15 @@ public interface EnumerationMethodConstants {
     public static final String ENUMERATION_QUERY_METHOD_OUTPUT_DESCRIPTION = "The enumerate response containing the EPR of the enumeration resource";
     public static final String ENUMERATION_RESPONSE_NAMESPACE = "http://gov.nih.nci.cagrid.enumeration/EnumerationResponseContainer";
     public static final QName ENUMERATION_QUERY_METHOD_OUTPUT_TYPE = new QName(ENUMERATION_RESPONSE_NAMESPACE, "EnumerationResponseContainer");
+    public static final QName ENUMERATION_QUERY_INPUT_MESSAGE = new QName(ENUMERATION_DATA_SERVICE_NAMESPACE, "EnumerationQueryRequest");
+    public static final QName ENUMERATION_QUERY_OUTPUT_MESSAGE = new QName(ENUMERATION_DATA_SERVICE_NAMESPACE, "EnumerationQueryResponse");
+    
     
     // CQL 2 enumeration query method constants
     public static final String CQL2_ENUMERATION_QUERY_METHOD_DESCRIPTION = "The standard caGrid Data Service query method which takes CQL 2 and begins an Enumeration";
     public static final String CQL2_ENUMERATION_QUERY_METHOD_NAME = "executeEnumerationQuery";
     public static final String CQL2_ENUMERATION_DATA_SERVICE_PACKAGE = ServiceNamingConstants.CQL2_DATA_SERVICE_PACKAGE + ".enumeration";
-    public static final String CQL2_ENUMERATION_DATA_SERVICE_NAMESPACE = "http://org.cagrid.dataservice.enumeration/Cql2EnumerationDataService";   
+    public static final String CQL2_ENUMERATION_DATA_SERVICE_NAMESPACE = "http://org.cagrid.dataservice.enumeration/Cql2EnumerationDataService";
+    public static final QName CQL2_ENUMERATION_QUERY_INPUT_MESSAGE = new QName(CQL2_ENUMERATION_DATA_SERVICE_NAMESPACE, "ExecuteEnumerationQueryRequest");
+    public static final QName CQL2_ENUMERATION_QUERY_OUTPUT_MESSAGE = new QName(CQL2_ENUMERATION_DATA_SERVICE_NAMESPACE, "ExecuteEnumerationQueryResponse");
 }
