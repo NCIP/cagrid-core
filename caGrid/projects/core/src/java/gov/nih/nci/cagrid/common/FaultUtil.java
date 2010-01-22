7afde1bf3c5ded962c567fb97f4f442a43832f99
jgeorge <jgeorge>
2010-01-22 14:55:20 +0000

    [CAGRID-230] Added a logFault method to FaultUtil
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15568 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/FaultUtil.java
+++ caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/FaultUtil.java
@@ -3,14 +3,13 @@ package gov.nih.nci.cagrid.common;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
 import org.oasis.wsrf.faults.BaseFaultType;
 
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
  * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
  * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
- * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
- *          Exp $
  */
 public class FaultUtil {
 	public static void printFault(Throwable e) {
@@ -42,6 +41,17 @@ public class FaultUtil {
 		} catch (Exception ex) {
 		}
 		return str;
+	}
+	
+	public static void logFault(Log log, Throwable e) {
+		if (e instanceof BaseFaultType) {
+			BaseFaultType fault = (BaseFaultType) e;
+			log.error(fault.getFaultString());
+			FaultHelper helper = new FaultHelper(fault);
+			log.error(helper.getStackTrace());
+		} else {
+			log.error(e, e);
+		}
 	}
 
 }
