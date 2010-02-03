5212ac7cea2a008241d47c3c2e3c17738df4e054
cmelean <cmelean>
2010-02-03 21:36:20 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15663 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/identifiers-namingauthority/src/org/cagrid/identifiers/namingauthority/IdentifierGenerator.java
+++
@@ -1,7 +0,0 @@
-package org.cagrid.identifiers.namingauthority;
-
-import java.net.URI;
-
-public interface IdentifierGenerator {
-	URI generate( NamingAuthorityConfig config );
-}
--- caGrid/projects/identifiers-namingauthority/src/org/cagrid/identifiers/namingauthority/NamingAuthorityConfig.java
+++
@@ -1,8 +0,0 @@
-package org.cagrid.identifiers.namingauthority;
-
-import java.net.URI;
-
-public interface NamingAuthorityConfig {
-	public URI getPrefix();
-	public String getGridSvcUrl();
-}
