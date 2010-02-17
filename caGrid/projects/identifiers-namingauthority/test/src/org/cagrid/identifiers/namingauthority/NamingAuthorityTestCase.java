f938f9fd709a4ca5ecbc4e22839eb38b198a8e80
cmelean <cmelean>
2010-02-17 21:40:31 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15791 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/identifiers-namingauthority/test/src/org/cagrid/identifiers/namingauthority/NamingAuthorityTestCase.java
+++ caGrid/projects/identifiers-namingauthority/test/src/org/cagrid/identifiers/namingauthority/NamingAuthorityTestCase.java
@@ -1,6 +1,9 @@
 package org.cagrid.identifiers.namingauthority;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.cagrid.identifiers.namingauthority.domain.IdentifierValues;
 import org.cagrid.identifiers.namingauthority.domain.KeyData;
@@ -67,6 +70,95 @@ public class NamingAuthorityTestCase extends NamingAuthorityTestCaseBase {
 			
 		assertResolvedValues(values);
 	}
+	
+	public void testGetKeys() {
+		IdentifierValues values = new IdentifierValues();
+		
+		values.put("URL", new KeyData( null, 
+				new String[]{"http://na.cagrid.org/foo", "http://na.cagrid.org/bar"} ));
+		
+		values.put("CODE", new KeyData( null,
+				new String[]{ "007" }));
+		
+		assertKeys(values);
+	}
+	
+	public void testGetKeyValues() {
+		IdentifierValues values = new IdentifierValues();
+		
+		values.put("URL", new KeyData( null, 
+				new String[]{"http://na.cagrid.org/foo", "http://na.cagrid.org/bar"} ));
+		
+		values.put("CODE", new KeyData( null,
+				new String[]{ "007" }));
+		
+		assertKeyValues(values, new String[]{ "URL", "CODE" });
+	}
+	
+	private void assertKeyValues(IdentifierValues values, String[] keyNames) {
+        URI id = null;
+        try {
+            id = this.NamingAuthority.createIdentifier(null, values);
+        } catch (NamingAuthorityConfigurationException e) {
+            e.printStackTrace();
+            fail("test configuration error");
+
+        } catch (InvalidIdentifierValuesException e) {
+            fail("Unexpected failure on creation:" + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("test configuration error");
+        }
+
+        String[] resolvedValues = null;
+
+        for(String key : keyNames) {
+        	try {
+        		resolvedValues = this.NamingAuthority.getKeyValues(null, id, key);
+        	} catch (NamingAuthorityConfigurationException e) {
+        		e.printStackTrace();
+        		fail("test configuration error");
+        	} catch (InvalidIdentifierException e) {
+        		fail("Unexpected failure on resolution:" + e.getMessage());
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        		fail("test configuration error");
+        	}
+        	List<String> inValues = values.getValues(key).getValues();
+        	assertEquals(inValues.toArray(new String[inValues.size()]), resolvedValues);
+        }
+    }
+	
+    private void assertKeys(IdentifierValues values) {
+        URI id = null;
+        try {
+            id = this.NamingAuthority.createIdentifier(null, values);
+        } catch (NamingAuthorityConfigurationException e) {
+            e.printStackTrace();
+            fail("test configuration error");
+
+        } catch (InvalidIdentifierValuesException e) {
+            fail("Unexpected failure on creation:" + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("test configuration error");
+        }
+
+        String[] resolvedKeys = null;
+
+        try {
+            resolvedKeys = this.NamingAuthority.getKeys(null, id);
+        } catch (NamingAuthorityConfigurationException e) {
+            e.printStackTrace();
+            fail("test configuration error");
+        } catch (InvalidIdentifierException e) {
+            fail("Unexpected failure on resolution:" + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("test configuration error");
+        }
+        assertEquals(values.getKeys(), resolvedKeys);
+    }
 
     private void assertResolvedValues(IdentifierValues values) {
         URI id = null;
@@ -97,5 +189,13 @@ public class NamingAuthorityTestCase extends NamingAuthorityTestCaseBase {
             fail("test configuration error");
         }
         assertEquals(values, resolvedValues);
+    }
+    
+    private void assertEquals(String[] arr1, String[] arr2) {
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        assertEquals(Arrays.asList(arr1), Arrays.asList(arr2));
     }
 }
