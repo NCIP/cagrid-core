c707a9b365580a98c3c2c005d7a1ab2c9767dc56
ervind <ervind>
2010-02-22 16:29:35 +0000

    much needed constants
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15843 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/cql/src/java/utils/org/cagrid/cql/utilities/CQLConstants.java
+++ caGrid/projects/cql/src/java/utils/org/cagrid/cql/utilities/CQLConstants.java
@@ -1,8 +1,17 @@
 package org.cagrid.cql.utilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.xml.namespace.QName;
 
-public interface CQLConstants {
+import org.cagrid.cql2.results.CQLAggregateResult;
+import org.cagrid.cql2.results.CQLAttributeResult;
+import org.cagrid.cql2.results.CQLObjectResult;
+import org.cagrid.cql2.results.CQLResult;
+import org.cagrid.cql2.results.ExtendedCQLResult;
+
+public class CQLConstants {
 
     public static final String CQL_NAMESPACE_URI = "http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery";
     public static final QName CQL_QUERY_QNAME = new QName(CQL_NAMESPACE_URI, "CQLQuery");
@@ -12,4 +21,30 @@ public interface CQLConstants {
     
     public static final String CQL2_RESULTS_NAMESPACE_URI = "http://CQL.caBIG/2/org.cagrid.cql2.results";
     public static final QName CQL2_RESULTS_QNAME = new QName(CQL2_RESULTS_NAMESPACE_URI, "CQLQueryResults");
+    
+    public static final Map<Class<? extends CQLResult>, QName> CQL2_RESULTS_QNAMES = 
+        new HashMap<Class<? extends CQLResult>, QName>();
+    
+    static {
+        CQL2_RESULTS_QNAMES.put(CQLObjectResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "CQLObjectResult"));
+        CQL2_RESULTS_QNAMES.put(CQLAttributeResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "CQLAttributeResult"));
+        CQL2_RESULTS_QNAMES.put(CQLAggregateResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "CQLAggregateResult"));
+        CQL2_RESULTS_QNAMES.put(ExtendedCQLResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "ExtendedCQLResult"));
+    }
+    
+    
+    public static final Map<Class<? extends CQLResult>, QName> CQL_RESULT_ELEMENT_QNAMES = 
+        new HashMap<Class<? extends CQLResult>, QName>();
+    
+    static {
+        CQL_RESULT_ELEMENT_QNAMES.put(CQLObjectResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "ObjectResult"));
+        CQL_RESULT_ELEMENT_QNAMES.put(CQLAttributeResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "AttributeResult"));
+        CQL_RESULT_ELEMENT_QNAMES.put(CQLAggregateResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "AggregateResult"));
+        CQL_RESULT_ELEMENT_QNAMES.put(ExtendedCQLResult.class, new QName(CQL2_RESULTS_NAMESPACE_URI, "ExtendedResult"));
+    }
+    
+    
+    private CQLConstants() {
+        // no instantiation, just constants
+    }
 }
