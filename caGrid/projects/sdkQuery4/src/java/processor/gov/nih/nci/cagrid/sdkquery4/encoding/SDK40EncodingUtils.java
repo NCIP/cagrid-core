2c765f9ae921c1737caf98ebd34afed0a13518fa
ervind <ervind>
2010-03-01 20:07:10 +0000

    picks up the castor mapping from server-config.wsdd
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@15947 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/sdkQuery4/src/java/processor/gov/nih/nci/cagrid/sdkquery4/encoding/SDK40EncodingUtils.java
+++ caGrid/projects/sdkQuery4/src/java/processor/gov/nih/nci/cagrid/sdkquery4/encoding/SDK40EncodingUtils.java
@@ -1,13 +1,19 @@
 package gov.nih.nci.cagrid.sdkquery4.encoding;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import gov.nih.nci.cagrid.common.Utils;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.axis.EngineConfiguration;
 import org.apache.axis.MessageContext;
+import org.apache.axis.WSDDEngineConfiguration;
+import org.apache.axis.deployment.wsdd.WSDDDeployment;
+import org.apache.axis.deployment.wsdd.WSDDService;
 import org.apache.axis.utils.ClassUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,120 +24,153 @@ import org.xml.sax.InputSource;
 
 public class SDK40EncodingUtils {
     // castor's DTD
-	public static final String CASTOR_MAPPING_DTD = "org/exolab/castor/mapping/mapping.dtd";//"mapping.dtd";
-	public static final String CASTOR_MAPPING_DTD_ENTITY = "-//EXOLAB/Castor Object Mapping DTD Version 1.0//EN";
-	public static final String DEFAULT_MARSHALLER_MAPPING = "/xml-mapping.xml";
-    public static final String DEFAULT_UNMARSHALLER_MAPPING = "/unmarshaller-xml-mapping.xml";
-	public static final String CASTOR_MARSHALLER_PROPERTY = "castorMarshallerMapping";
-    public static final String CASTOR_UNMARSHALLER_PROPERTY = "castorUnmarshallerMapping";
-    
-	protected static Log LOG = LogFactory.getLog(SDK40EncodingUtils.class.getName());
+    public static final String CASTOR_MAPPING_DTD = "org/exolab/castor/mapping/mapping.dtd";
+    public static final String CASTOR_MAPPING_DTD_ENTITY = "-//EXOLAB/Castor Object Mapping DTD Version 1.0//EN";
 
-	// maps <mapping location> to bytes of the Mapping file
-	// using Hashtable for synchronization correctness
-	protected static Map<String, byte[]> mappingCacheMap = new HashMap<String, byte[]>();
-    
+    // properties expected to be defined in global section of wsdd
+    public static final String CASTOR_MARSHALLER_PROPERTY = "castorMarshallerMapping";
+    public static final String CASTOR_UNMARSHALLER_PROPERTY = "castorUnmarshallerMapping";
+
+    // default filenames / locations of [un]marshalling mapping documents
+    public static final String DEFAULT_MARSHALLER_MAPPING = "/xml-mapping.xml";
+    public static final String DEFAULT_UNMARSHALLER_MAPPING = "/unmarshaller-xml-mapping.xml";
+
+    protected static Log LOG = LogFactory.getLog(SDK40EncodingUtils.class.getName());
+
+    // maps resource location to the contents of the resource
+    protected static Map<String, String> resourceMap = Collections.synchronizedMap(new HashMap<String, String>());
+
+    protected static EntityResolver dtdResolver = null;
+
+
     public static Mapping getMarshallerMapping(MessageContext context) {
         return getMapping(context, CASTOR_MARSHALLER_PROPERTY);
     }
-    
-    
+
+
     public static Mapping getUnmarshallerMapping(MessageContext context) {
         return getMapping(context, CASTOR_UNMARSHALLER_PROPERTY);
     }
 
 
-	private static Mapping getMapping(MessageContext context, String mappingProperty) {
-		long startTime = System.currentTimeMillis();
+    protected static Mapping getMapping(MessageContext context, String mappingProperty) {
+        long startTime = System.currentTimeMillis();
 
-		EntityResolver resolver = new EntityResolver() {
-			public InputSource resolveEntity(String publicId, String systemId) {
-				if (publicId.equals(CASTOR_MAPPING_DTD_ENTITY)) {
-					InputStream in = ClassUtils.getResourceAsStream(SDK40EncodingUtils.class, CASTOR_MAPPING_DTD);
-					return new InputSource(in);
-				}
-				return null;
-			}
-		};
-
-        // determine the mapping location, starting with a default based on the property
-		String mappingLocation = mappingProperty.equals(CASTOR_MARSHALLER_PROPERTY) 
-            ? DEFAULT_MARSHALLER_MAPPING : DEFAULT_UNMARSHALLER_MAPPING;
-		if (context != null) {
-			String prop = (String) context.getProperty(mappingProperty);
-			if (prop != null && !prop.trim().equals("")) {
-                // the property exists in the message context, use the property value
-				mappingLocation = prop;
-				LOG.debug("Loading castor mapping from message context property[" + mappingProperty + "]");
-			} else {
-				try {
-                    // attempt to find the property in the wsdd global configuration
-					prop = (String) context.getAxisEngine().getConfig().getGlobalOptions().get(mappingProperty);
-				} catch (Exception e) {
-					LOG.debug("Error reading global configuration:" + e.getMessage(), e);
-				}
-				if (prop != null && !prop.trim().equals("")) {
-					mappingLocation = prop;
-					LOG.debug("Loading castor mapping from globalConfiguration property[" + mappingProperty + "]");
-				} else {
-					LOG.debug("Unable to locate castor mapping property[" + mappingProperty
-						+ "], using default mapping location:" + mappingLocation);
-				}
-			}
-		} else {
-			LOG.debug("Unable to determine message context, using default mapping location:" + mappingLocation);
-		}
+        // determine the mapping location, starting with a default based on the
+        // property
+        String mappingLocation = mappingProperty.equals(CASTOR_MARSHALLER_PROPERTY)
+            ? DEFAULT_MARSHALLER_MAPPING
+            : DEFAULT_UNMARSHALLER_MAPPING;
+        if (context != null) {
+            String prop = (String) context.getProperty(mappingProperty);
+            if (prop != null && !prop.trim().equals("")) {
+                // the property exists in the message context, use the property
+                // value
+                mappingLocation = prop;
+                LOG.debug("Loading castor mapping from message context property[" + mappingProperty + "]");
+            } else {
+                try {
+                    // attempt to find the property in the wsdd global
+                    // configuration
+                    prop = (String) context.getAxisEngine().getConfig().getGlobalOptions().get(mappingProperty);
+                } catch (Exception e) {
+                    LOG.warn("Error reading global configuration:" + e.getMessage(), e);
+                }
+                if (prop != null && !prop.trim().equals("")) {
+                    mappingLocation = prop;
+                    LOG.debug("Loading castor mapping from globalConfiguration property[" + mappingProperty + "]");
+                } else {
+                    // walk through the WSDD config and find the property in
+                    // service config options
+                    EngineConfiguration config = context.getAxisEngine().getConfig();
+                    if (config instanceof WSDDEngineConfiguration) {
+                        WSDDDeployment wsdd = ((WSDDEngineConfiguration) config).getDeployment();
+                        WSDDService[] services = wsdd.getServices();
+                        for (WSDDService service : services) {
+                            prop = service.getParameter(mappingProperty);
+                            if (prop != null && !prop.trim().equals("")) {
+                                // found it!
+                                mappingLocation = prop;
+                                LOG.debug("Loading castor mapping from service " + service.getQName() 
+                                    + " property [" + prop + "]");
+                                break;
+                            }
+                        }
+                    }
+                    if (!(prop != null && !prop.trim().equals(""))) {
+                        LOG.warn("Unable to locate castor mapping property[" + mappingProperty
+                            + "], using default mapping location:" + mappingLocation);
+                    }
+                }
+            }
+        } else {
+            LOG.debug("Unable to determine message context, using default mapping location:" + mappingLocation);
+        }
 
         // locate the bytes of the mapping file
-        byte[] mappingBytes = null;
-		if (mappingCacheMap.containsKey(mappingLocation)) {
-			LOG.debug("Loading Mapping from cache for location:" + mappingLocation);
-            mappingBytes = mappingCacheMap.get(mappingLocation);
-		} else {
-			LOG.debug("Unable to loading Mapping from cache for location:" + mappingLocation);
-			LOG.debug("Attempting to load mapping from mapping location:" + mappingLocation);
-			InputStream mappingStream = ClassUtils.getResourceAsStream(SDK40EncodingUtils.class, mappingLocation);
-			if (mappingStream == null) {
-				LOG.error("Mapping file [" + mappingLocation + "] was null!");
-			} else {
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                byte[] buffer = new byte[8192];
-                int len = -1;
-                try {
-                    while ((len = mappingStream.read(buffer)) != -1) {
-                        bos.write(buffer, 0, len);
-                    }
-                    mappingBytes = bos.toByteArray();
-                    mappingCacheMap.put(mappingLocation, mappingBytes);
-                    mappingStream.close();
-                } catch (IOException ex) {
-                    LOG.error("Error loading mapping file [" + mappingLocation + "] : "
-                        + ex.getMessage(), ex);
-                }
-			}
-		}
-        
-        ByteArrayInputStream mappingStream = new ByteArrayInputStream(mappingBytes);
-        Mapping mapping = loadMappingFromStream(mappingLocation, mappingStream, resolver);
-        
-		long duration = System.currentTimeMillis() - startTime;
-		LOG.debug("Time to load mapping file:" + duration + " ms.");
+        String mappingDocument = null;
+        try {
+            mappingDocument = loadResource(mappingLocation);
+        } catch (IOException ex) {
+            LOG.error("Error loading mapping file [" + mappingLocation + "] : " + ex.getMessage(), ex);
+        }
 
-		return mapping;
-	}
-    
-    
-    private static Mapping loadMappingFromStream(String mappingLocation,
-            InputStream mappingStream, EntityResolver resolver) {
-        InputSource mappIS = new org.xml.sax.InputSource(mappingStream);
+        Mapping mapping = loadMappingFromString(mappingLocation, mappingDocument, getDtdResolver());
+
+        long duration = System.currentTimeMillis() - startTime;
+        LOG.debug("Time to load mapping file:" + duration + " ms.");
+
+        return mapping;
+    }
+
+
+    protected static Mapping loadMappingFromString(String mappingLocation, String mappingContents,
+        EntityResolver resolver) {
+        InputSource mappIS = new org.xml.sax.InputSource(new StringReader(mappingContents));
         Mapping mapping = new Mapping();
         mapping.setEntityResolver(resolver);
         try {
             mapping.loadMapping(mappIS);
         } catch (Exception ex) {
-            LOG.error("Error loading castor mapping (" 
-                + mappingLocation + "): " + ex.getMessage(), ex);
+            LOG.error("Error loading castor mapping (" + mappingLocation + "): " + ex.getMessage(), ex);
         }
         return mapping;
+    }
+
+
+    protected static String loadResource(String resourceName) throws IOException {
+        String resource = null;
+        synchronized (resourceMap) {
+            if (resourceMap.containsKey(resourceName)) {
+                resource = resourceMap.get(resourceName);
+            } else {
+                InputStream stream = ClassUtils.getResourceAsStream(SDK40EncodingUtils.class, resourceName);
+                if (stream != null) {
+                    StringBuffer buffer = Utils.inputStreamToStringBuffer(stream);
+                    resource = buffer.toString();
+                    resourceMap.put(resourceName, resource);
+                    stream.close();
+                }
+            }
+        }
+        return resource;
+    }
+
+
+    protected static EntityResolver getDtdResolver() {
+        if (dtdResolver == null) {
+            // simple entity resolver to load the castor dtd from the class
+            // loader
+            dtdResolver = new EntityResolver() {
+                public InputSource resolveEntity(String publicId, String systemId) throws IOException {
+                    if (publicId.equals(CASTOR_MAPPING_DTD_ENTITY)) {
+                        String dtd = loadResource(CASTOR_MAPPING_DTD);
+                        return new InputSource(new StringReader(dtd));
+                    }
+                    return null;
+                }
+            };
+        }
+        return dtdResolver;
     }
 }
