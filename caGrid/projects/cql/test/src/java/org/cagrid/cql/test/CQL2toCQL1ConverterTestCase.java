75688e60a6ef0b5eedecb908a57999a2468ef4cf
ervind <ervind>
2010-06-28 17:16:28 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16292 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/cql/test/src/java/org/cagrid/cql/test/CQL2toCQL1ConverterTestCase.java
@@ -0,0 +1,50 @@
+package org.cagrid.cql.test;
+
+import org.cagrid.cql.utilities.CQL2toCQL1Converter;
+import org.cagrid.cql.utilities.QueryConversionException;
+import org.cagrid.cql2.Aggregation;
+import org.cagrid.cql2.CQLQuery;
+import org.cagrid.cql2.CQLQueryModifier;
+import org.cagrid.cql2.DistinctAttribute;
+
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class CQL2toCQL1ConverterTestCase extends TestCase {
+    
+    public CQL2toCQL1ConverterTestCase(String name) {
+        super(name);
+    }
+    
+    
+    public void testConvertUnsupportedAggregation() {
+        CQLQuery query = new CQLQuery();
+        CQLQueryModifier mods = new CQLQueryModifier();
+        DistinctAttribute da = new DistinctAttribute();
+        da.setAttributeName("name");
+        da.setAggregation(Aggregation.MAX);
+        mods.setDistinctAttribute(da);
+        query.setCQLQueryModifier(mods);
+        
+        try {
+            CQL2toCQL1Converter.convertToCql1Query(query);
+            // shouldn't work
+            fail("Conversion of aggregation " + Aggregation.MAX.getValue() + " to CQL 1 should have failed");
+        } catch (QueryConversionException ex) {
+            // expected
+        } catch (Exception ex) {
+            // not expected
+            ex.printStackTrace();
+            fail("Unexpected error converting CQL 2 to 1: " + ex.getMessage());
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        TestRunner runner = new TestRunner();
+        TestResult result = runner.doRun(new TestSuite(CQL2toCQL1ConverterTestCase.class));
+        System.exit(result.errorCount() + result.failureCount());
+    }
+}
