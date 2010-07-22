6290699503853c8bbc5da16afa3c27ebbbaf78b2
ervind <ervind>
2010-07-22 04:23:18 +0000

    CAGRID-335: The promised step to turn off CQL 2
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16455 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ integration-tests/projects/fqpTests/src/org/cagrid/fqp/test/common/steps/DisableCql2QueryProcessorStep.java
@@ -0,0 +1,39 @@
+package org.cagrid.fqp.test.common.steps;
+
+import gov.nih.nci.cagrid.common.PropertiesPreservingComments;
+import gov.nih.nci.cagrid.data.QueryProcessorConstants;
+import gov.nih.nci.cagrid.testing.system.haste.Step;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Edits the service.properties file of an Introduce
+ * generated data service to clear out the CQL 2
+ * query processor class name property, thereby disabling
+ * the CQL 2 functionality of a data service
+ * 
+ * @author David W. Ervin
+ */
+public class DisableCql2QueryProcessorStep extends Step {
+    
+    private String baseDir = null;
+
+    public DisableCql2QueryProcessorStep(String baseDir) {
+        super();
+        this.baseDir = baseDir;
+    }
+
+
+    public void runStep() throws Throwable {
+        File servicePropsFile = new File(baseDir, "service.properties");
+        assertTrue("Service properties file (" + servicePropsFile.getAbsolutePath() + ") not found", servicePropsFile.exists());
+        PropertiesPreservingComments props = new PropertiesPreservingComments();
+        props.load(servicePropsFile);
+        props.setProperty(QueryProcessorConstants.CQL2_QUERY_PROCESSOR_CLASS_PROPERTY, "");
+        FileOutputStream out = new FileOutputStream(servicePropsFile);
+        props.store(out);
+        out.flush();
+        out.close();
+    }
+}
