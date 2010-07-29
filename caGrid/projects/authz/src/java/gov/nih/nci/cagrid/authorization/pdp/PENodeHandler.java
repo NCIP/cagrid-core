d69a83cf1b1ebb6b31a5af09cde56c68fe1ad022
ervind <ervind>
2010-07-29 14:04:53 +0000

    CAGRID-462, CAGRID-463: Removing authz and gridftpauthz
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_release/Software/core@16529 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/authz/src/java/gov/nih/nci/cagrid/authorization/pdp/PENodeHandler.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.cagrid.authorization.pdp;
-
-import org.w3c.dom.Node;
-
-public interface PENodeHandler {
-	
-	public void handleNode(PENode node);
-
-}
--- caGrid/projects/authz/src/java/gov/nih/nci/cagrid/authorization/pdp/PENodeSelectorSelector.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.cagrid.authorization.pdp;
-
-import org.apache.axis.MessageContext;
-
-public interface PENodeSelectorSelector {
-	
-	PENodeSelector select(MessageContext context);
-
-}
