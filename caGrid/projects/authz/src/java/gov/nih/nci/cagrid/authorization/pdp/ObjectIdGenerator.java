ce322f2bfcf5bcc1b9f71fdb74711dfd68cc8c18
ervind <ervind>
2010-07-29 16:17:25 +0000

    CAGRID-462, CAGRID-463
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16534 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/authz/src/java/gov/nih/nci/cagrid/authorization/pdp/ObjectIdGenerator.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.cagrid.authorization.pdp;
-
-import org.w3c.dom.Node;
-
-public interface ObjectIdGenerator {
-	
-	String generateId(Node node);
-
-}
--- caGrid/projects/authz/src/java/gov/nih/nci/cagrid/authorization/pdp/PENode.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.cagrid.authorization.pdp;
-
-import org.w3c.dom.Node;
-
-public interface PENode {
-	Node getNode();
-	String getObjectId();
-}
