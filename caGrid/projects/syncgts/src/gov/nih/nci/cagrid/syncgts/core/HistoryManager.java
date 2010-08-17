a96f28736eb0c1030aa2dc6c263245b288f321f3
ervind <ervind>
2010-08-17 14:32:11 +0000

    fixing GForge [#29359] SyncGTS HistoryManager fails to truncate history logs in .cagrid/syncgts/history
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16605 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/syncgts/src/gov/nih/nci/cagrid/syncgts/core/HistoryManager.java
+++ caGrid/projects/syncgts/src/gov/nih/nci/cagrid/syncgts/core/HistoryManager.java
@@ -5,61 +5,67 @@ import gov.nih.nci.cagrid.syncgts.bean.DateFilter;
 import gov.nih.nci.cagrid.syncgts.bean.SyncReport;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.xml.namespace.QName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
  * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
  * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @author <A href="mailto:David.Ervin@osumc.edu">David W. Ervin</A>
  * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
  *          Exp $
  */
 public class HistoryManager {
 
-	private final static QName reportQN = new QName(SyncGTSDefault.SYNC_GTS_NAMESPACE, "SyncReport");
-	public static int maxSyncReports = 150;
-
+	public static final int DEFAULT_MAX_REPORTS = 150;
+	public static final String MAX_REPORTS_PROPERTY = "max.sync.reports";
+	
+	private static final QName REPORTS_QN = new QName(SyncGTSDefault.SYNC_GTS_NAMESPACE, "SyncReport");
+	
+	private static Log LOG = LogFactory.getLog(HistoryManager.class);
 
 	public File addReport(SyncReport report) throws Exception {
 		File r = getFile(report.getTimestamp());
-		Utils.serializeDocument(r.getAbsolutePath(), report, reportQN);
+		Utils.serializeDocument(r.getAbsolutePath(), report, REPORTS_QN);
 		return r;
 	}
+	
+	
+	public SyncReport getReport(String fileName) throws Exception {
+        return Utils.deserializeDocument(fileName, SyncReport.class);
+    }
 
 
 	public SyncReport getLastReport() throws Exception {
-		File dir = getLastestDayDir();
+		File dir = getLastDayDir();
 		if (dir != null) {
-			File latest = null;
-			int num = -1;
-			File files[] = dir.listFiles();
-			for (int i = 0; i < files.length; i++) {
-				if (files[i].isFile()) {
-
-					int index = files[i].getName().indexOf(".xml");
-					if (index != -1) {
-						try {
-							int temp = Integer.valueOf(files[i].getName().substring(0, index)).intValue();
-							if (temp > num) {
-								num = temp;
-								latest = files[i];
-							}
-						} catch (NumberFormatException e) {
-
-						}
-					}
-
-				}
+			File last = null;
+			if (dir != null && dir.exists()) {
+	            File[] files = dir.listFiles(new FileFilter() {
+	                public boolean accept(File pathname) {
+	                    return pathname.getName().endsWith(".xml");
+	                }
+	            });
+	            if (files != null) {
+	                sortByFilename(files);
+	                last = files[files.length - 1];
+	            }
+	        }
+			if (last != null) {
+				return getReport(last.getAbsolutePath());
 			}
-			if (latest != null) {
-				return getReport(latest.getAbsolutePath());
-			}
-
 		}
 		return null;
 	}
@@ -80,79 +86,62 @@ public class HistoryManager {
 	}
 
 
+	/**
+	 * Gets the earliest named sub-directory
+	 * 
+	 * @param dir
+	 * @return
+	 */
 	private File getEarliestDir(File dir) {
-		File earliest = null;
-		int num = Integer.MAX_VALUE;
-		if (dir != null) {
-			if (dir.exists() && dir.isDirectory()) {
-				File[] dirs = dir.listFiles();
-				for (int i = 0; i < dirs.length; i++) {
-					if (dirs[i].isDirectory()) {
-						try {
-							int temp = Integer.valueOf(dirs[i].getName()).intValue();
-							if (temp < num) {
-								num = temp;
-								earliest = dirs[i];
-							}
-						} catch (NumberFormatException e) {
-
-						}
-					}
-				}
-
-			}
-		}
-		return earliest;
+	    File earliest = null;
+	    if (dir != null && dir.exists()) {
+	        File[] dirs = dir.listFiles(new FileFilter() {
+	            public boolean accept(File pathname) {
+	                return pathname.isDirectory();
+	            }
+	        });
+	        if (dirs != null) {
+	            sortByFilename(dirs);
+	            earliest = dirs[0];
+	        }
+	    }
+	    return earliest;
 	}
 
 
-	private File getLastestDayDir() {
-		return getLatestDir(getLatestMonthDir());
+	private File getLastDayDir() {
+		return getLastDir(getLastMonthDir());
 	}
 
 
-	private File getLatestMonthDir() {
-		return getLatestDir(getLatestYearDir());
+	private File getLastMonthDir() {
+		return getLastDir(getLastYearDir());
 	}
 
 
-	private File getLatestYearDir() {
-		return getLatestDir(getHistoryDirectory());
+	private File getLastYearDir() {
+		return getLastDir(getHistoryDirectory());
 	}
 
 
-	private File getLatestDir(File dir) {
-		File latest = null;
-		int num = -1;
-		if (dir != null) {
-			if (dir.exists() && dir.isDirectory()) {
-				File[] dirs = dir.listFiles();
-				for (int i = 0; i < dirs.length; i++) {
-					if (dirs[i].isDirectory()) {
-						try {
-							int temp = Integer.valueOf(dirs[i].getName()).intValue();
-							if (temp > num) {
-								num = temp;
-								latest = dirs[i];
-							}
-						} catch (NumberFormatException e) {
-
-						}
-					}
-				}
-
-			}
-		}
-		return latest;
+	private File getLastDir(File dir) {
+		File last = null;
+		if (dir != null && dir.exists()) {
+            File[] dirs = dir.listFiles(new FileFilter() {
+                public boolean accept(File pathname) {
+                    return pathname.isDirectory();
+                }
+            });
+            if (dirs != null) {
+                sortByFilename(dirs);
+                last = dirs[dirs.length - 1];
+            }
+        }
+		return last;
 	}
 
 
-	public SyncReport getReport(String fileName) throws Exception {
-		return (SyncReport) Utils.deserializeDocument(fileName, SyncReport.class);
-	}
-
-
-	private DateFilter getEarliestFitler() throws Exception {
+	private DateFilter getEarliestDateFilter() throws Exception {
 		File day = getEarliestDayDir();
 		File month = day.getParentFile();
 		File year = month.getParentFile();
@@ -168,72 +157,30 @@ public class HistoryManager {
 		}
 	}
 
+	
 	private boolean isAfter(DateFilter start, DateFilter end){
-		if(start.getYear()<=end.getYear()){
-			if(start.getMonth()<=end.getMonth()){
-				if(start.getDay()<=end.getDay()){
+	    if (start.getYear() <= end.getYear()) {
+			if (start.getMonth() <= end.getMonth()) {
+				if (start.getDay() <= end.getDay()) {
 					return false;
-				}else{
+				} else {
 					return true;
 				}
-			}else{
+			} else {
 				return true;
 			}
-		}else{
+		} else {
 			return true;
 		}
 	}
-
-	public void prune(DateFilter filter) throws Exception {
-		DateFilter start = getEarliestFitler();
-		if(start==null){
-			return;
-		}
-
-		Calendar c = new GregorianCalendar();
-		c.add(Calendar.YEAR, (filter.getYear() * -1));
-		c.add(Calendar.MONTH, (filter.getMonth() * -1));
-		c.add(Calendar.DAY_OF_MONTH, (filter.getDay() * -1));
-		DateFilter end = new DateFilter();
-		end.setDay(c.get(Calendar.DAY_OF_MONTH));
-		end.setMonth(c.get(Calendar.MONTH) + 1);
-		end.setYear(c.get(Calendar.YEAR));
-
-		while (!isAfter(start, end)) {
-			File startDir = getDirectory(start);
-			if ((startDir.exists()) && (startDir.isDirectory())) {
-				File[] fileList = startDir.listFiles();
-				for (int i = 0; i < fileList.length; i++) {
-					fileList[i].delete();
-				}
-				File monthDir = startDir.getParentFile();
-				File yearDir = monthDir.getParentFile();
-				startDir.delete();
-				deleteIfEmpty(monthDir);
-				deleteIfEmpty(yearDir);
-
-			}
-			this.incrementDate(start);
-		}
-	}
-
-
-	private void deleteIfEmpty(File dir) {
-		if (dir.isDirectory()) {
-			File files[] = dir.listFiles();
-			if ((files == null) || (files.length == 0)) {
-				dir.delete();
-			}
-		}
-	}
-
+	
 
 	public SyncReport[] search(DateFilter startDate, DateFilter end) throws Exception {
 		DateFilter start = new DateFilter();
 		start.setDay(startDate.getDay());
 		start.setMonth(startDate.getMonth());
 		start.setYear(startDate.getYear());
-		SyncReport[] reports = new SyncReport[maxSyncReports];
+		SyncReport[] reports = new SyncReport[DEFAULT_MAX_REPORTS];
 		int checkMax = 0;
 		int iterator = 0;
 		this.incrementDate(end);
@@ -242,7 +189,7 @@ public class HistoryManager {
 			if ((startDir.exists()) && (startDir.isDirectory())) {
 				String[] fileList = startDir.list();
 				checkMax = checkMax + fileList.length;
-				if (checkMax > maxSyncReports)
+				if (checkMax > DEFAULT_MAX_REPORTS)
 					throw new Exception();
 				else {
 					for (int i = 0; i < fileList.length; i++) {
@@ -272,7 +219,7 @@ public class HistoryManager {
 		start.setDay(startDate.getDay());
 		start.setMonth(startDate.getMonth());
 		start.setYear(startDate.getYear());
-		SyncReport[] reports = new SyncReport[maxSyncReports];
+		SyncReport[] reports = new SyncReport[DEFAULT_MAX_REPORTS];
 		int checkMax = 0;
 		int iterator = 0;
 		this.incrementDate(end);
@@ -281,7 +228,7 @@ public class HistoryManager {
 			if ((startDir.exists()) && (startDir.isDirectory())) {
 				String[] fileList = startDir.list();
 				checkMax = checkMax + fileList.length;
-				if (checkMax > maxSyncReports)
+				if (checkMax > DEFAULT_MAX_REPORTS)
 					throw new Exception();
 				else {
 					for (int i = 0; i < fileList.length; i++) {
@@ -291,8 +238,9 @@ public class HistoryManager {
 							reports[iterator] = this.getReport(startDir.getAbsolutePath() + File.separator
 								+ fileList[i]);
 							iterator++;
-						} else
+						} else {
 							throw new Exception();
+						}
 						in.close();
 					}
 				}
@@ -312,7 +260,7 @@ public class HistoryManager {
 	}
 
 
-	private File getHistoryDirectory() {
+	protected File getHistoryDirectory() {
 		File dir = new File(SyncGTSDefault.getSyncGTSUserDir() + File.separator + "history");
 		dir.mkdirs();
 		return dir;
@@ -321,21 +269,7 @@ public class HistoryManager {
 
 	private File getDirectory(DateFilter f) {
 		File histDir = getHistoryDirectory();
-		String month;
-		String day;
-		if (f.getMonth() < 10) {
-			month = "0" + f.getMonth();
-		} else
-			month = "" + f.getMonth();
-
-		if (f.getDay() < 10) {
-			day = "0" + f.getDay();
-		} else
-			day = "" + f.getDay();
-
-		File dir = new File(histDir.getAbsolutePath() + File.separator + f.getYear() + File.separator + month
-			+ File.separator + day);
-		return dir;
+		return getDirectory(f, histDir);
 	}
 
 
@@ -344,16 +278,18 @@ public class HistoryManager {
 		String day;
 		if (f.getMonth() < 10) {
 			month = "0" + f.getMonth();
-		} else
+		} else {
 			month = "" + f.getMonth();
+		}
 
 		if (f.getDay() < 10) {
 			day = "0" + f.getDay();
-		} else
+		} else {
 			day = "" + f.getDay();
+		}
 
-		File dir = new File(histDir.getAbsolutePath() + File.separator + f.getYear() + File.separator + month
-			+ File.separator + day);
+		File dir = new File(histDir, 
+		    f.getYear() + File.separator + month + File.separator + day);
 		return dir;
 	}
 
@@ -363,25 +299,175 @@ public class HistoryManager {
 		String month = timestamp.substring(4, 6);
 		String days = timestamp.substring(6, 8);
 		File histDir = getHistoryDirectory();
-		File dir = new File(histDir.getAbsolutePath() + File.separator + year + File.separator + month + File.separator
-			+ days);
+		File dir = new File(histDir,
+		    year + File.separator + month + File.separator + days);
 		dir.mkdirs();
 		return dir;
 	}
 
 
 	private void incrementDate(DateFilter f) {
-		if (f.getDay() >= 31) {
-			f.setDay(1);
-			if (f.getMonth() >= 12) {
-				f.setMonth(1);
-				f.setYear(f.getYear() + 1);
-			} else {
-				f.setMonth(f.getMonth() + 1);
-			}
-		} else
-			f.setDay(f.getDay() + 1);
-
+	    Calendar cal = Calendar.getInstance();
+	    // lenient calendar since the old implementation allowed
+	    // conditions like "Feb 31st" to exist...
+	    cal.setLenient(true);
+	    // set up the current date
+	    cal.set(Calendar.YEAR, f.getYear());
+	    cal.set(Calendar.MONTH, f.getMonth() - 1);
+	    cal.set(Calendar.DAY_OF_MONTH, f.getDay());
+	    // move forward one day
+	    cal.roll(Calendar.DAY_OF_MONTH, 1);
+	    
+	    // put the date back into the DateFilter bean
+	    f.setDay(cal.get(Calendar.DAY_OF_MONTH));
+	    f.setMonth(cal.get(Calendar.MONTH + 1));
+	    f.setYear(cal.get(Calendar.YEAR));
+	}
+	
+	
+	private void sortByFilename(File[] files) {
+	    Comparator<File> comp = new Comparator<File>() {
+            public int compare(File o1, File o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        Arrays.sort(files, comp);
+	}
+	
+	
+	public void prune(DateFilter filter) throws Exception {
+	    // filter tells us how old of a history file we can keep
+	    // create a calendar of the current date
+	    Calendar startCal = Calendar.getInstance();
+	    // roll it BACK by the ammount in the filter
+	    startCal.roll(Calendar.YEAR, -filter.getYear());
+	    startCal.roll(Calendar.MONTH, -filter.getMonth());
+	    startCal.roll(Calendar.DAY_OF_MONTH, -filter.getDay());
+	    // create the cuttoff date
+	    DateFilter cuttoff = new DateFilter(
+	        startCal.get(Calendar.DAY_OF_MONTH),
+	        startCal.get(Calendar.MONTH) + 1,
+	        startCal.get(Calendar.YEAR));
+	    // list all the history files and dirs in date order
+        List<File> historyFiles = listHistoryFilesByDate();
+        for (File f : historyFiles) {
+            if (f.isFile() && f.getName().endsWith(".xml")) {
+                // if a history file, figure its creation date
+                File dayDir = f.getParentFile();
+                File monthDir = dayDir.getParentFile();
+                File yearDir = monthDir.getParentFile();
+                DateFilter current = new DateFilter(
+                    Integer.parseInt(dayDir.getName()),
+                    Integer.parseInt(monthDir.getName()),
+                    Integer.parseInt(yearDir.getName()));
+                if (!isAfter(current, cuttoff)) {
+                    // file is from before the cuttoff date and can be deleted
+                    f.delete();
+                    LOG.info("Pruned old sync report " + f.getAbsolutePath());
+                }
+            }
+        }
+        
+        // clean up any empty directories
+        for (File f : historyFiles) {
+            if (f.exists() && f.isDirectory()) {
+                List<File> contents = Utils.recursiveListFiles(f, new FileFilter() {
+                    public boolean accept(File pathname) {
+                        return true;
+                    }
+                });
+                boolean empty = true;
+                for (File content : contents) {
+                    if (content.isFile()) {
+                        empty = false;
+                    }
+                }
+                if (empty) {
+                    Utils.deleteDir(f);
+                }
+            }
+        }
+        /*
+        int maxHistory = getMaxReports();
+        // get the number of reports down to the acceptable max
+        boolean deletedSomething = true;
+        while ((countXmlDocs(historyFiles) > maxHistory) && deletedSomething) {
+            deletedSomething = false;
+            // TOO MANY!
+            for (File f : historyFiles) {
+                if (f.isFile() && f.getName().endsWith(".xml")) {
+                    // see if the file is older than the requested cache size
+                    
+                    f.delete();
+                    deletedSomething = true;
+                    break;
+                }
+            }
+        }
+        // clean up any empty directories
+        for (File f : historyFiles) {
+            if (f.isDirectory()) {
+                deleteIfEmpty(f);
+            }
+        }
+        */
+    }
+	
+	
+	private List<File> listHistoryFilesByDate() {
+	    final File historyDir = getHistoryDirectory();
+	    List<File> historyFiles = Utils.recursiveListFiles(historyDir, new FileFilter() {
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".xml") || pathname.isDirectory();
+            }
+        });
+	    Comparator<File> comp = new Comparator<File>() {
+            public int compare(File o1, File o2) {
+                int val = 0;
+                try {
+                    String rel1 = Utils.getRelativePath(historyDir, o1);
+                    String rel2 = Utils.getRelativePath(historyDir, o2);
+                    val = rel1.compareTo(rel2);
+                } catch (Exception ex) {
+                    // uhoh
+                }
+                return val;
+            }
+        };
+        Collections.sort(historyFiles, comp);
+        return historyFiles;
+	}
+	
+	
+	private int countXmlDocs(List<File> files) {
+	    int count = 0;
+	    for (File f : files) {
+	        if (f.isFile() && f.getName().endsWith(".xml")) {
+	            count++;
+	        }
+	    }
+	    return count;
 	}
 
+
+    private void deleteIfEmpty(File dir) {
+        if (dir.isDirectory()) {
+            File files[] = dir.listFiles();
+            if ((files == null) || (files.length == 0)) {
+                dir.delete();
+            }
+        }
+    }
+	
+	
+	private static int getMaxReports() {
+	    int max = DEFAULT_MAX_REPORTS;
+	    String val = System.getProperty(MAX_REPORTS_PROPERTY);
+	    try {
+	        max = Integer.valueOf(val).intValue();
+	    } catch (Exception ex) {
+	        // whatever
+	    }
+	    return max;
+	}
 }
