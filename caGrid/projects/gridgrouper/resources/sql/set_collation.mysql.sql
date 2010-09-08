d888a3951762f1fabc29a85a0f051046fb1dc0e4
jgeorge <jgeorge>
2010-09-08 20:23:02 +0000

    Putting the sql statements needed to install, upgrade, and configure gridgrouper into sql files and svn.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_release/Software/core@16709 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/gridgrouper/resources/sql/set_collation.mysql.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `grouper_members` CHANGE `subject_id` `subject_id` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL 
---
+++ caGrid/projects/gridgrouper/resources/sql/set_innodb_engine.mysql.sql
@@ -0,0 +1,16 @@
+ALTER TABLE gridgrouper_membershiprequest ENGINE = InnoDB;
+ALTER TABLE gridgrouper_membershiprequest_history ENGINE = InnoDB;
+ALTER TABLE grouper_attributes ENGINE = InnoDB;
+ALTER TABLE grouper_composites ENGINE = InnoDB;
+ALTER TABLE grouper_fields ENGINE = InnoDB;
+ALTER TABLE grouper_groups ENGINE = InnoDB;
+ALTER TABLE grouper_groups_types ENGINE = InnoDB;
+ALTER TABLE grouper_members ENGINE = InnoDB;
+ALTER TABLE grouper_memberships ENGINE = InnoDB;
+ALTER TABLE grouper_owners ENGINE = InnoDB;
+ALTER TABLE grouper_sessions ENGINE = InnoDB;
+ALTER TABLE grouper_settings ENGINE = InnoDB;
+ALTER TABLE grouper_stems ENGINE = InnoDB;
+ALTER TABLE grouper_types ENGINE = InnoDB;
+ALTER TABLE subject ENGINE = InnoDB;
+ALTER TABLE subjectattribute ENGINE = InnoDB;
