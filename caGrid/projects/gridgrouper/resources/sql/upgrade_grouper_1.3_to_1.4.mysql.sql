74c96e283c70d5fe22e7a11d240ca5e17d48567f
jgeorge <jgeorge>
2010-09-08 20:18:39 +0000

    Putting the sql statements needed to install, upgrade, and configure gridgrouper into sql files and svn.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16708 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/gridgrouper/resources/sql/upgrade_grouper_1.3_to_1.4.mysql.sql
@@ -0,0 +1,33 @@
+create table gridgrouper_membershiprequest (
+	id varchar(128) not null,
+	requestor varchar(255) not null,
+	group_id varchar(128) not null,
+	request_time bigint,
+	status varchar(255) not null,
+	reviewer_id varchar(128),
+	review_time bigint,
+	public_note varchar(255),
+	admin_note varchar(255),
+	primary key (id)
+);
+
+create table gridgrouper_membershiprequest_history (
+	id varchar(128) not null,
+	membershiprequest_id varchar(128) not null,
+	status varchar(255) not null,
+	reviewer_id varchar(128),
+	update_date bigint,
+	public_note varchar(255),
+	admin_note varchar(255),
+	primary key (id)
+);
+
+
+alter table gridgrouper_membershiprequest_history add index FKF6C38EB551626995 (reviewer_id), add constraint FKF6C38EB551626995 foreign key (reviewer_id) references grouper_members (id);
+alter table gridgrouper_membershiprequest_history add index FKF6C38EB53847521 (membershiprequest_id), add constraint FKF6C38EB53847521 foreign key (membershiprequest_id) references gridgrouper_membershiprequest (id);
+
+alter table gridgrouper_membershiprequest add index FKCF5D636051626995 (reviewer_id), add constraint FKCF5D636051626995 foreign key (reviewer_id) references grouper_members (id);
+alter table gridgrouper_membershiprequest add index FKCF5D63601E2E76DB (group_id), add constraint FKCF5D63601E2E76DB foreign key (group_id) references grouper_groups (owner_id);
+
+alter table gridgrouper_membershiprequest ENGINE = InnoDB;
+alter table gridgrouper_membershiprequest_history ENGINE = InnoDB;
