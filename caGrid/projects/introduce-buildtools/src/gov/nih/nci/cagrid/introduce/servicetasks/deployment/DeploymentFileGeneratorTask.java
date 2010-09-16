141768e33124de902e759043b3c058d7738257be
ervind <ervind>
2010-09-16 18:31:10 +0000

    CAGRID-404: For reasons unknown, Globus and / or Axis hang when [de]serializing stuff when the process is launched by the caGrid installer under Windows XP SP3.  These changes switch over to use JDom and directly build and manipulate XML.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@16721 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/introduce-buildtools/src/gov/nih/nci/cagrid/introduce/servicetasks/deployment/DeploymentFileGeneratorTask.java
+++ caGrid/projects/introduce-buildtools/src/gov/nih/nci/cagrid/introduce/servicetasks/deployment/DeploymentFileGeneratorTask.java
@@ -1,66 +1,72 @@
 package gov.nih.nci.cagrid.introduce.servicetasks.deployment;
 
-import gov.nih.nci.cagrid.introduce.servicetasks.beans.deployment.Deployment;
-import gov.nih.nci.cagrid.introduce.servicetasks.beans.deployment.Jar;
-import gov.nih.nci.cagrid.introduce.servicetasks.beans.deployment.Jars;
+import gov.nih.nci.cagrid.common.XMLUtilities;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Properties;
 
 import javax.xml.namespace.QName;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
-import org.globus.wsrf.encoding.ObjectSerializer;
+import org.jdom.Element;
 
-
+/**
+ * DeploymentFileGeneratorTask
+ *  * 
+ * <note>This class used to serialize a bean to handle this, however that causes
+ * problems when the code is executed out of the caGrid installer under
+ * Windows XP sp 3.  For more details, see https://jira.citih.osumc.edu/browse/CAGRID-404</note>
+ * 
+ * @author David
+ */
 public class DeploymentFileGeneratorTask extends Task {
 
     public static final String DEPLOYMENT_PERSISTENCE_FILE = "introduceDeployment.xml";
     public static final QName DEPLOYMENT_PERSISTENCE_QNAME = new QName(
         "gme://gov.nih.nci.cagrid.introduce/1/Deployment", "Deployment");
-
-
+    
     public void execute() throws BuildException {
         super.execute();
         
-
-        Properties properties = new Properties();
-        properties.putAll(this.getProject().getProperties());
-        
+        // get the base directory
         String baseDir =  this.getProject().getBaseDir().getAbsolutePath();
-
-        Deployment deployment = new Deployment();
-        deployment.setServiceName(properties.getProperty("introduce.skeleton.service.name"));
-        deployment.setDeploymentPrefix(properties.getProperty("service.deployment.prefix"));
-        deployment.setServiceDeploymentDirName(properties.getProperty("service.deployment.dir.name"));
-        deployment.setDeployDateTime(String.valueOf(System.currentTimeMillis()));
+        
+        // create the Deployment element
+        Element deployment = new Element("Deployment", DEPLOYMENT_PERSISTENCE_QNAME.getNamespaceURI());
+        deployment.setAttribute("serviceName", getProject().getProperty("introduce.skeleton.service.name"));
+        deployment.setAttribute("deploymentPrefix", getProject().getProperty("service.deployment.prefix"));
+        deployment.setAttribute("serviceDeploymentDirName", getProject().getProperty("service.deployment.dir.name"));
+        deployment.setAttribute("deployDateTime", String.valueOf(System.currentTimeMillis()));
         
         // get the list of jars to be deployed and add them
-        File libDir = new File(properties.getProperty("build.lib.dir"));
+        File libDir = new File(getProject().getProperty("build.lib.dir"));
         String[] jars = libDir.list();
-
-        Jars deployedJars = new Jars();
-
-        Jar[] jarArr = new Jar[jars.length];
+        
+        // create the Jars element
+        Element deployedJars = new Element("Jars", DEPLOYMENT_PERSISTENCE_QNAME.getNamespaceURI());
         for (int i = 0; i < jars.length; i++) {
             String jarFile = jars[i];
-            Jar newJar = new Jar();
-            newJar.setName(jarFile);
-            newJar.setLocation(".");
-            jarArr[i] = newJar;
+            Element newJar = new Element("Jar", DEPLOYMENT_PERSISTENCE_QNAME.getNamespaceURI());
+            newJar.setAttribute("name", jarFile);
+            newJar.setAttribute("location", ".");
+            deployedJars.addContent(newJar);
         }
-        deployedJars.setJar(jarArr);
-        deployment.setJars(deployedJars);
         
+        // add the jars element to the deployment element
+        deployment.addContent(deployedJars);
 
+        // write it out
         FileWriter fw = null;
         try {
-            fw = new FileWriter(baseDir + File.separator +  "tmp" + File.separator + "etc" + File.separator + DEPLOYMENT_PERSISTENCE_FILE);
-            ObjectSerializer.serialize(fw, deployment, DEPLOYMENT_PERSISTENCE_QNAME );
+            File out = new File(baseDir, "tmp" + File.separator + "etc" + File.separator + DEPLOYMENT_PERSISTENCE_FILE);
+            fw = new FileWriter(out);
+            String xml = XMLUtilities.elementToString(deployment);
+            xml = XMLUtilities.formatXML(xml);
+            fw.write(xml);
             fw.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new BuildException(e);
         }
     }
