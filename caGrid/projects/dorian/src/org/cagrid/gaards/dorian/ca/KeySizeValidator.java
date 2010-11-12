59b4d1a761954b0ddc01aa93b407fe178934a80c
ervind <ervind>
2010-11-12 19:28:05 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@16916 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/dorian/src/org/cagrid/gaards/dorian/ca/KeySizeValidator.java
+++ caGrid/projects/dorian/src/org/cagrid/gaards/dorian/ca/KeySizeValidator.java
@@ -1,13 +1,26 @@
 package org.cagrid.gaards.dorian.ca;
 
+/**
+ * KeySizeValidator
+ * Validates that the specified key size is supported
+ * 
+ * @author David
+ */
 public class KeySizeValidator {
+    
+    public static final int[] VALID_KEY_SIZES = {
+        512, 1024, 2048
+    };
 
     public static boolean isKeySizeValid(int keySize) {
-        if ((keySize == 512) || (keySize == 1024) || (keySize == 2048)) {
-            return true;
-        } else {
-            return false;
+        boolean valid = false;
+        for (int size : VALID_KEY_SIZES) {
+            if (size == keySize) {
+                valid = true;
+                break;
+            }
         }
+        return valid;
     }
 
 }
