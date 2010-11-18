026b0fd44ab1c72727b65b7b6c9bb5292bee4f33
ervind <ervind>
2010-11-18 21:21:26 +0000

    CAGRID-543: need some utilities
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@16987 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/SSLDebug.java
+++ caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/SSLDebug.java
@@ -7,15 +7,18 @@ public class SSLDebug {
         StringBuffer hex = new StringBuffer();
         hex.append(message).append("\n");
         String rawHex = Utils.bytesToHex(cert);
-        for (int i = 0; i < rawHex.length(); i++) {
+        for (int i = 0; i < rawHex.length(); i += 2) {
             if (i != 0 && (i % 12) == 0) {
                 hex.append("\n");
             }
-            hex.append(rawHex.charAt(i));
-            if ((i % 2) == 0) {
-                hex.append(" ");
-            }
+            hex.append(rawHex.charAt(i)).append(rawHex.charAt(i + 1)).append(" ");
         }
         return hex.toString();
+    }
+    
+    
+    public static void main(String[] args) {
+        byte[] wut = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(debugSSL("Hi!", wut));
     }
 }
