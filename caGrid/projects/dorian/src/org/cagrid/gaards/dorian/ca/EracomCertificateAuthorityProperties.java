eedfd6c5da31f61e30f229d820d1f86df2af715f
ervind <ervind>
2010-11-23 15:23:31 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@17001 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/dorian/src/org/cagrid/gaards/dorian/ca/EracomCertificateAuthorityProperties.java
+++ caGrid/projects/dorian/src/org/cagrid/gaards/dorian/ca/EracomCertificateAuthorityProperties.java
@@ -1,24 +1,23 @@
 package org.cagrid.gaards.dorian.ca;
 
+import org.cagrid.gaards.core.EracomUtils;
 import org.cagrid.gaards.dorian.common.Lifetime;
 import org.cagrid.gaards.dorian.stubs.types.DorianInternalFault;
 
 
 public class EracomCertificateAuthorityProperties extends CertificateAuthorityProperties {
-    public static final String DEFAULT_ERACOM_SIGNATURE_ALGORITHM = "SHA256WithRSA";
-
     private int slot;
 
 
     public EracomCertificateAuthorityProperties(String certificateAuthorityPassword, int issuedCertificateKeySize,
         int slot) throws DorianInternalFault {
-        this(certificateAuthorityPassword, null, DEFAULT_ERACOM_SIGNATURE_ALGORITHM, issuedCertificateKeySize, false, null, false, null, slot);
+        this(certificateAuthorityPassword, null, EracomUtils.getEracomCryptoAlgorithm(), issuedCertificateKeySize, false, null, false, null, slot);
     }
 
 
     public EracomCertificateAuthorityProperties(String certificateAuthorityPassword, String policyOID,
         int issuedCertificateKeySize, int slot) throws DorianInternalFault {
-        this(certificateAuthorityPassword, policyOID, DEFAULT_ERACOM_SIGNATURE_ALGORITHM, issuedCertificateKeySize, false, null, false, null, slot);
+        this(certificateAuthorityPassword, policyOID, EracomUtils.getEracomCryptoAlgorithm(), issuedCertificateKeySize, false, null, false, null, slot);
     }
 
 
