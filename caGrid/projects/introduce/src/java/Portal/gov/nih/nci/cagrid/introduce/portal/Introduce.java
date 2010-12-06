c662ca769f620b35a3d4ce4788b14fbbafa35dfa
mgrand <mgrand>
2010-12-06 16:25:42 +0000

    Improved fatal error handling during start-up.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@17082 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/Introduce.java
+++ caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/Introduce.java
@@ -130,6 +130,12 @@ public final class Introduce {
 
             if (confFile == null) {
                 confFile = IntroducePropertiesManager.getIntroduceConfigurationFile();
+                if (confFile == null || confFile.trim().length() == 0) {
+                    String msg = "No value is configured for " + IntroduceConstants.INTRODUCE_CONFIGURATION_FILE + " in "
+                            + IntroduceConstants.INTRODUCE_PROPERTIES;
+                    logger.fatal(msg);
+                    throw new RuntimeException(msg);
+                }
             }
 
             Application app = (Application) Utils.deserializeDocument(confFile, Application.class);
@@ -149,7 +155,8 @@ public final class Introduce {
             applicationInstance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         } catch (Exception e) {
-            // TODO Auto-generated catch block
+            String msg = "Introduce UI failed to launch";
+            logger.fatal(msg, e);
             e.printStackTrace();
         }
     }
