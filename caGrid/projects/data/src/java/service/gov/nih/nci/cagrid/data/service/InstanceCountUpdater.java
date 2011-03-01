f4173450eb98e178d62991d923f2be6fe869fe9b
ervind <ervind>
2011-03-01 19:52:15 +0000

    CAGRID-629: Don't bother with the timer task unless all the required information is present
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@17418 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/service/InstanceCountUpdater.java
+++ caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/service/InstanceCountUpdater.java
@@ -52,60 +52,81 @@ public class InstanceCountUpdater {
      */
     public static void startCountUpdateTask(final DomainModel model, final CQL2QueryProcessor processor, 
         final Resource baseResource, final Method setterMethod, int updateFrequency) {
-        TimerTask task = new TimerTask() {
-            public void run() {
-                LOG.debug("Starting instance count update");
-                DataServiceInstanceCounts counts = new DataServiceInstanceCounts();
-                if (model != null) {
-                    UMLClass[] modelClasses = model.getExposedUMLClassCollection().getUMLClass();
-                    if (modelClasses != null) {
-                        InstanceCount[] instanceCounts = new InstanceCount[modelClasses.length];
-                        LOG.debug("Found " + instanceCounts.length + " classes to count");
-                        for (int i = 0; i < modelClasses.length; i++) {
-                            String className = DomainModelUtils.getQualifiedClassname(modelClasses[i]);
-                            LOG.debug("Counting instances of " + className);
-                            long count = -1;
-                            try {
-                                count = processor.getInstanceCount(className);
-                                LOG.debug("Found " + count + " instances");
-                            } catch (QueryProcessingException ex) {
-                                LOG.warn("Error obtaining a count of " + className + ": " + ex.getMessage(), ex);
+        boolean shouldStart = false;
+        // verify we have everything we need before setting up the task
+        if (model == null) {
+            LOG.warn("Refusing to start up instance count task with no domain model.");
+            shouldStart = false;
+        }
+        if (processor == null) {
+            LOG.warn("Refusing to start up instance count task with no query processor.");
+            shouldStart = false;
+        }
+        if (baseResource == null) {
+            LOG.warn("Refusing to start up instance count task with no base resource.");
+            shouldStart = false;
+        }
+        if (setterMethod == null) {
+            LOG.warn("Refusing to start up instance count task with no resource property setter method.");
+            shouldStart = false;
+        }
+        if (shouldStart) {
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    LOG.debug("Starting instance count update");
+                    DataServiceInstanceCounts counts = new DataServiceInstanceCounts();
+                    if (model != null) {
+                        UMLClass[] modelClasses = model.getExposedUMLClassCollection().getUMLClass();
+                        if (modelClasses != null) {
+                            InstanceCount[] instanceCounts = new InstanceCount[modelClasses.length];
+                            LOG.debug("Found " + instanceCounts.length + " classes to count");
+                            for (int i = 0; i < modelClasses.length; i++) {
+                                String className = DomainModelUtils.getQualifiedClassname(modelClasses[i]);
+                                LOG.debug("Counting instances of " + className);
+                                long count = -1;
+                                try {
+                                    count = processor.getInstanceCount(className);
+                                    LOG.debug("Found " + count + " instances");
+                                } catch (QueryProcessingException ex) {
+                                    LOG.warn("Error obtaining a count of " + className + ": " + ex.getMessage(), ex);
+                                }
+                                instanceCounts[i] = new InstanceCount(className, count);
+                                try {
+                                    Thread.sleep(SLEEP_BETWEEN_QUERIES);
+                                } catch (InterruptedException ex) {
+                                    // whatever
+                                }
                             }
-                            instanceCounts[i] = new InstanceCount(className, count);
-                            try {
-                                Thread.sleep(SLEEP_BETWEEN_QUERIES);
-                            } catch (InterruptedException ex) {
-                                // whatever
-                            }
+                            counts.setInstanceCount(instanceCounts);                    
+                        } else {
+                            LOG.debug("No classes in the model; no counts to update");
                         }
-                        counts.setInstanceCount(instanceCounts);                    
                     } else {
-                        LOG.debug("No classes in the model; no counts to update");
+                        LOG.error("No domain model found; can't update instance counts!");
                     }
-                } else {
-                    LOG.error("No domain model found; can't update instance counts!");
-                }
-                counts.setLastUpdated(Calendar.getInstance());
-                if (LOG.isDebugEnabled()) {
+                    counts.setLastUpdated(Calendar.getInstance());
+                    if (LOG.isDebugEnabled()) {
+                        try {
+                            StringWriter writer = new StringWriter();
+                            Utils.serializeObject(counts, MetadataConstants.DATA_INSTANCE_QNAME, writer);
+                            LOG.debug("Here's the instance count document:\n" + writer.getBuffer().toString());
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    LOG.debug("Setting the count on the resource property");
                     try {
-                        StringWriter writer = new StringWriter();
-                        Utils.serializeObject(counts, MetadataConstants.DATA_INSTANCE_QNAME, writer);
-                        LOG.debug("Here's the instance count document:\n" + writer.getBuffer().toString());
+                        setterMethod.invoke(baseResource, counts);
                     } catch (Exception ex) {
-                        ex.printStackTrace();
+                        LOG.error("Error invoking setter for the instance count resource property: " + ex.getMessage(), ex);
                     }
                 }
-                LOG.debug("Setting the count on the resource property");
-                try {
-                    setterMethod.invoke(baseResource, counts);
-                } catch (Exception ex) {
-                    LOG.error("Error invoking setter for the instance count resource property: " + ex.getMessage(), ex);
-                }
-            }
-        };
-        
-        // TODO: may have to make the timer use a daemon thread
-        Timer t = new Timer(InstanceCountUpdater.class.getName() + " task");
-        t.schedule(task, 0, updateFrequency * 1000);
+            };
+            // TODO: may have to make the timer use a daemon thread
+            Timer t = new Timer(InstanceCountUpdater.class.getName() + " task");
+            t.schedule(task, 0, updateFrequency * 1000);
+        } else {
+            LOG.info("Instance count task will not be started");
+        }
     }
 }
