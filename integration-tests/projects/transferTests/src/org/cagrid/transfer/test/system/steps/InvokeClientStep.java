3d70d49aa596d122bfeed0cb96f420468939f33c
ervind <ervind>
2011-05-11 15:58:36 +0000

    CAGRID-687: dump process output to the console
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@17759 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- integration-tests/projects/transferTests/src/org/cagrid/transfer/test/system/steps/InvokeClientStep.java
+++ integration-tests/projects/transferTests/src/org/cagrid/transfer/test/system/steps/InvokeClientStep.java
@@ -3,6 +3,8 @@ package org.cagrid.transfer.test.system.steps;
 import java.io.File;
 import java.util.List;
 
+import org.apache.axis.message.addressing.EndpointReferenceType;
+
 import gov.nih.nci.cagrid.introduce.common.AntTools;
 import gov.nih.nci.cagrid.introduce.common.CommonTools;
 import gov.nih.nci.cagrid.introduce.test.TestCaseInfo;
@@ -14,7 +16,6 @@ public class InvokeClientStep extends BaseStep {
     public static final String TEST_URL_SUFFIX = "/wsrf/services/cagrid/";
 
     private TestCaseInfo tci;
-    private String methodName;
     private ServiceContainer container;
 
 
@@ -29,19 +30,15 @@ public class InvokeClientStep extends BaseStep {
         System.out.println("Invoking a simple methods implementation.");
 
         List<String> cmd = AntTools.getAntCommand("runClient", tci.getDir());
-        String urlArg = "-Dservice.url=";
+        EndpointReferenceType serviceEPR = container.getServiceEPR("cagrid/" + tci.getName());
+        String urlArg = "-Dservice.url=" + serviceEPR.getAddress().toString();
+        cmd.add(urlArg);
         if (container.getProperties().isSecure()) {
-            urlArg += "https://";
             String certdir = "-DX509_CERT_DIR=" + container.getProperties().getContainerDirectory().getAbsolutePath() + File.separator + "certificates" + File.separator + "ca";
             cmd.add(certdir);
-        } else {
-            urlArg += "http://";
         }
-        urlArg += "localhost:" + container.getProperties().getPortPreference().getPort() + TEST_URL_SUFFIX + tci.getName();
-        cmd.add(urlArg);
         
-        
-        Process p = CommonTools.createAndOutputProcess(cmd);
+        Process p = CommonTools.createAndOutputProcess(cmd, System.out, System.err);
         p.waitFor();
 
         assertTrue("ant runClient did not successfully complete!", p.exitValue() == 0);
