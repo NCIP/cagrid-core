a93898eca5985652fc6e84dcd723980ef32da9fa
sbokhari <sbokhari>
2011-05-20 14:05:52 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_release/Software/core@17804 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/installer/src/org/cagrid/installer/tasks/UnzipInstallTask.java
+++ caGrid/projects/installer/src/org/cagrid/installer/tasks/UnzipInstallTask.java
@@ -70,7 +70,11 @@ public class UnzipInstallTask extends BasicTask {
 
         File installDir = new File(model.getProperty(this.installDirPathProp));
         File home = new File(installDir, model.getProperty(this.dirNameProp));
-
+        if(home.exists()) 
+		{
+        		deleteDir(home);
+			home.mkdir();
+		}
         model.setProperty(this.homeProp, home.getAbsolutePath());
 
         Enumeration entries = zipFile.entries();
@@ -116,4 +120,18 @@ public class UnzipInstallTask extends BasicTask {
 
         return null;
     }
+    public static boolean deleteDir(File dir) 
+	{
+	    if (dir.isDirectory()) 
+	    {
+	        String[] children = dir.list();
+	        for (int i=0; i<children.length; i++) 
+	        {
+	            boolean success = deleteDir(new File(dir, children[i]));
+	            if (!success) {return false;}
+	        }
+	    }
+	    // The directory is now empty so delete it
+	    return dir.delete();
+	}
 }
