33b5a40203d744300ddd4f81cfee05f51a11c57e
ervind <ervind>
2011-05-20 17:29:56 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@17808 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- integration-tests/projects/transferTests/src/org/cagrid/transfer/test/system/TransferServiceTest.java
+++ integration-tests/projects/transferTests/src/org/cagrid/transfer/test/system/TransferServiceTest.java
@@ -13,6 +13,7 @@ import gov.nih.nci.cagrid.testing.system.deployment.steps.StartContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.StopContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.UnpackContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.story.ServiceStoryBase;
+import gov.nih.nci.cagrid.testing.system.haste.Step;
 
 import java.io.File;
 import java.util.Arrays;
@@ -35,10 +36,11 @@ public class TransferServiceTest extends ServiceStoryBase {
 
 
     public TransferServiceTest(ServiceContainer container) {
-       super(container);
-       PropertyConfigurator.configure("." + File.separator + "conf" + File.separator + "log4j.properties");
+        super(container);
+        PropertyConfigurator.configure("." + File.separator + "conf" + File.separator + "log4j.properties");
     }
-    
+
+
     public TransferServiceTest() {
         PropertyConfigurator.configure("." + File.separator + "conf" + File.separator + "introduce" + File.separator
             + "log4j.properties");
@@ -56,44 +58,42 @@ public class TransferServiceTest extends ServiceStoryBase {
         return getDescription();
     }
 
+
     public String getDescription() {
-        if(getContainer().getProperties().isSecure()){
+        if (getContainer().getProperties().isSecure()) {
             return "Secure Transfer Service Test";
         }
         return "Transfer Service Test";
     }
 
 
-    protected Vector steps() {
-        Vector steps = new Vector();
+    protected Vector<Step> steps() {
+        Vector<Step> steps = new Vector<Step>();
         try {
             steps.add(new UnpackContainerStep(getContainer()));
-            List<String> deploymentArgs = 
-                Arrays.asList(new String[] {"-Dno.deployment.validation=true"});
+            List<String> deploymentArgs = Arrays.asList(new String[]{"-Dno.deployment.validation=true"});
             steps.add(new DeployServiceStep(getContainer(), "../transfer", deploymentArgs));
 
             steps.add(new CreateSkeletonStep(tci, false));
-            steps.add(new AddCreateTransferMethodStep(tci,getContainer(), false));
+            steps.add(new AddCreateTransferMethodStep(tci, getContainer(), false));
             steps.add(new AddCreateTransferMethodImplStep(tci, false));
             if (getContainer() instanceof SecureContainer) {
                 steps.add(new CopyProxyStep((SecureContainer) getContainer(), tci));
-                steps.add(new CopyCAStep((SecureContainer)getContainer(), tci));
+                steps.add(new CopyCAStep((SecureContainer) getContainer(), tci));
             }
             steps.add(new DeployServiceStep(getContainer(), tci.getDir()));
             steps.add(new StartContainerStep(getContainer()));
 
             steps.add(new InvokeClientStep(getContainer(), tci));
-            
+
             steps.add(new StopContainerStep(getContainer()));
-            
-            steps.add(new AddCreateStreamingTransferMethodStep(tci,getContainer(), false));
+
+            steps.add(new AddCreateStreamingTransferMethodStep(tci, getContainer(), false));
             steps.add(new AddCreateStreamingTransferMethodImplStep(tci, false));
             steps.add(new DeployServiceStep(getContainer(), tci.getDir()));
             steps.add(new StartContainerStep(getContainer()));
 
             steps.add(new InvokeClientStep(getContainer(), tci));
-            
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -102,8 +102,6 @@ public class TransferServiceTest extends ServiceStoryBase {
 
 
     protected boolean storySetUp() throws Throwable {
-        
-
         RemoveSkeletonStep step1 = new RemoveSkeletonStep(tci);
         try {
             step1.runStep();
@@ -134,5 +132,4 @@ public class TransferServiceTest extends ServiceStoryBase {
             e.printStackTrace();
         }
     }
-
 }
