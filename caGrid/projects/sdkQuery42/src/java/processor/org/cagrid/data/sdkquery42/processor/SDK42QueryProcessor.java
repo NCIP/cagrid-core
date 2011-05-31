bfe66f329a813ca62f1e30ccf3f2b06e52cf723d
jgeorge <jgeorge>
2011-05-31 18:33:43 +0000

    CAGRID-703 - Corrected the isUseHttps method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@17870 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/sdkQuery42/src/java/processor/org/cagrid/data/sdkquery42/processor/SDK42QueryProcessor.java
+++ caGrid/projects/sdkQuery42/src/java/processor/org/cagrid/data/sdkquery42/processor/SDK42QueryProcessor.java
@@ -203,7 +203,7 @@ public class SDK42QueryProcessor extends CQLQueryProcessor {
         boolean useHttps = Boolean.parseBoolean(DEFAULT_HOST_HTTPS);
         String useHttpsValue = getConfiguredParameters().getProperty(PROPERTY_HOST_HTTPS);
         try {
-            useHttps = Boolean.parseBoolean(PROPERTY_HOST_HTTPS);
+            useHttps = Boolean.parseBoolean(useHttpsValue);
         } catch (Exception ex) {
             LOG.error("Error parsing property " + PROPERTY_HOST_HTTPS
                 + ".  Value was " + useHttpsValue, ex);
