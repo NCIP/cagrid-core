924a3d46e7ba8531ff0b9b9e552cdd94f97283a3
ervind <ervind>
2011-06-21 18:47:55 +0000

    CAGRIDKC-806: An example project to get developers started with a CQL 2 query processor that resolves dependencies from Ivy
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@17896 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ template-projects/ExampleCustomQueryProcessor/src/org/cagrid/example/CustomQueryProcessor.java
@@ -0,0 +1,30 @@
+package org.cagrid.example;
+
+import gov.nih.nci.cagrid.data.MalformedQueryException;
+import gov.nih.nci.cagrid.data.QueryProcessingException;
+import gov.nih.nci.cagrid.data.cql2.CQL2QueryProcessor;
+
+import org.cagrid.cql2.CQLQuery;
+import org.cagrid.cql2.results.CQLQueryResults;
+
+public class CustomQueryProcessor extends CQL2QueryProcessor {
+
+
+    @Override
+    public CQLQueryResults processQuery(CQLQuery query) throws QueryProcessingException, MalformedQueryException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    /*
+     * You might want to implement this method if you want a memory-efficient implementation that can
+     * iterate a stream of results rather than having to create the full result set at once.
+     * 
+     * This is more important for cases using WS-Enumeration or caGrid Transfer
+    public Iterator<CQLResult> processQueryAndIterate(CQLQuery query) throws QueryProcessingException, MalformedQueryException {
+        CQLQueryResults results = processQuery(query);
+        return new ResultsIterator(results);
+    }
+     */
+}
