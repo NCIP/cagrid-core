f37d02711e685270c3b0bbcb72747e5af4e7e330
ervind <ervind>
2011-06-24 19:51:43 +0000

    CAGRID-716: Smarter cycle test ignores the test classes dir
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@17900 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/cql/test/src/java/org/cagrid/cql/test/CQL2QueryResultsIteratorTestCase.java
+++ caGrid/projects/cql/test/src/java/org/cagrid/cql/test/CQL2QueryResultsIteratorTestCase.java
@@ -1,14 +1,20 @@
 package org.cagrid.cql.test;
 
-import java.util.NoSuchElementException;
+import gov.nih.nci.cagrid.common.Utils;
 
-import org.cagrid.cql.utilities.iterator.CQL2QueryResultsIterator;
-import org.cagrid.cql2.results.CQLQueryResults;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
 
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
+import org.cagrid.cql.utilities.iterator.CQL2QueryResultsIterator;
+import org.cagrid.cql2.results.CQLAttributeResult;
+import org.cagrid.cql2.results.CQLQueryResults;
+import org.cagrid.cql2.results.TargetAttribute;
 
 
 public class CQL2QueryResultsIteratorTestCase extends TestCase {
@@ -29,6 +35,94 @@ public class CQL2QueryResultsIteratorTestCase extends TestCase {
             assertTrue("Unexpected exception type (" + ex.getClass().getName() + ") thrown", 
                 ex instanceof NoSuchElementException);
         }
+        try {
+            iter.remove();
+            fail("Should not have been able to remove from the iterator");
+        } catch (Exception ex) {
+            assertTrue("Unexpected exception type (" + ex.getClass().getName() + ") thrown",
+                ex instanceof UnsupportedOperationException);
+        }
+    }
+    
+    
+    public void testIterateAttributes() {
+        int resultsCount = 100;
+        int targetAttributeCount = 2;
+        CQLQueryResults results = generateAttributeResults(resultsCount, targetAttributeCount);
+        CQL2QueryResultsIterator iter = new CQL2QueryResultsIterator(results);
+        int foundCount = 0;
+        assertTrue("Iterator should have had results, but hasNext() was false", iter.hasNext());
+        while (iter.hasNext()) {
+            foundCount++;
+            Object item = iter.next();
+            assertNotNull("Item returned from the iterator was null", item);
+            assertEquals("Object returned from the iterator should have been a TargetAttribute[]", 
+                TargetAttribute[].class, item.getClass());
+            TargetAttribute[] tas = (TargetAttribute[]) item;
+            assertEquals("Unexpected number of target attributes found", targetAttributeCount, tas.length);
+        }
+        assertEquals("Unexpected number of results returned from the iterator", resultsCount, foundCount);
+        try {
+            iter.remove();
+            fail("Should not have been able to remove from the iterator");
+        } catch (Exception ex) {
+            assertTrue("Unexpected exception type (" + ex.getClass().getName() + ") thrown",
+                ex instanceof UnsupportedOperationException);
+        }
+    }
+    
+    
+    public void testIterateAttributesXml() {
+        int resultsCount = 100;
+        int targetAttributeCount = 2;
+        CQLQueryResults results = generateAttributeResults(resultsCount, targetAttributeCount);
+        CQL2QueryResultsIterator iter = new CQL2QueryResultsIterator(results, true);
+        int foundCount = 0;
+        assertTrue("Iterator should have had results, but hasNext() was false", iter.hasNext());
+        while (iter.hasNext()) {
+            foundCount++;
+            Object item = iter.next();
+            assertNotNull("Item returned from the iterator was null", item);
+            assertEquals("Object returned from the iterator should have been a String", 
+                String.class, item.getClass());
+            String xml = (String) item;
+            InputStream wsdd = getClass().getResourceAsStream("/org/cagrid/cql2/mapping/client-config.wsdd");
+            assertNotNull("Couldn't get the CQL 2 client config wsdd", wsdd);
+            try {
+                CQLAttributeResult attrib = Utils.deserializeObject(new StringReader(xml), CQLAttributeResult.class, wsdd);
+                assertNotNull("Attribute result was null", attrib);
+                TargetAttribute[] tas = attrib.getAttribute();
+                assertNotNull("Target Attributes returned were null", tas);
+                assertEquals("Unexpected number of target attributes found", targetAttributeCount, tas.length);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                fail("Error deserializing attribute results: " + ex.getMessage());
+            }
+        }
+        assertEquals("Unexpected number of results returned from the iterator", resultsCount, foundCount);
+        try {
+            iter.remove();
+            fail("Should not have been able to remove from the iterator");
+        } catch (Exception ex) {
+            assertTrue("Unexpected exception type (" + ex.getClass().getName() + ") thrown",
+                ex instanceof UnsupportedOperationException);
+        }
+    }
+    
+    
+    private CQLQueryResults generateAttributeResults(int resultsCount, int numTargetAttributes) {
+        CQLQueryResults results = new CQLQueryResults();
+        results.setAttributeResult(new CQLAttributeResult[resultsCount]);
+        for (int i = 0; i < resultsCount; i++) {
+            CQLAttributeResult ar = new CQLAttributeResult();
+            ar.setAttribute(new TargetAttribute[numTargetAttributes]);
+            for (int j = 0; j < numTargetAttributes; j++) {
+                TargetAttribute ta = new TargetAttribute("attribute_" + j + "_" + i, "value_" + j + "_" + i);
+                ar.setAttribute(j, ta);
+            }
+            results.setAttributeResult(i, ar);
+        }
+        return results;
     }
     
     
