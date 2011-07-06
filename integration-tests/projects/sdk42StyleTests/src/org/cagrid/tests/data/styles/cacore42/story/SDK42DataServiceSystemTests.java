603998ae51a5db9379fc3e83382c9d1c20f76733
jgeorge <jgeorge>
2011-07-06 19:49:10 +0000

    Broke the various SDK42 tests out to more easily determine which test is failing
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_SHA2/Software/core@17947 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- integration-tests/projects/sdk42StyleTests/src/org/cagrid/tests/data/styles/cacore42/story/SDK42DataServiceSystemTests.java
+++ integration-tests/projects/sdk42StyleTests/src/org/cagrid/tests/data/styles/cacore42/story/SDK42DataServiceSystemTests.java
@@ -10,18 +10,18 @@ import junit.textui.TestRunner;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SDK42DataServiceSystemTests {
     
     private static Log LOG = LogFactory.getLog(SDK42DataServiceSystemTests.class);
     
-    private long lastTime = 0;
-    private File tempApplicationDir = null;
+    private static File tempApplicationDir;
     
-    @Test
-    public void sdk42DataServiceSystemTests() throws Throwable {
+    @BeforeClass
+    public static void setUp() throws Throwable {
         // create a temporary directory for the SDK application to package things in
         tempApplicationDir = File.createTempFile("SdkExample", "temp");
         tempApplicationDir.delete();
@@ -29,73 +29,68 @@ public class SDK42DataServiceSystemTests {
         LOG.debug("Created temp application base dir: " + tempApplicationDir.getAbsolutePath());
         
         // throw out the ivy cache dirs used by the SDK
-        splitTime();
         LOG.debug("Destroying SDK ivy cache");
         NukeIvyCacheStory nukeStory = new NukeIvyCacheStory();
         nukeStory.runBare();
-        
+
         // create the caCORE SDK example project
-        splitTime();
         LOG.debug("Running caCORE SDK example project creation story");
         CreateExampleProjectStory createExampleStory = new CreateExampleProjectStory(tempApplicationDir, false);
         createExampleStory.runBare();
-        
+    }
+    
+    @Test
+    public void localSDKApiTests() throws Throwable {
         // create and run a caGrid Data Service using the SDK's local API
-        splitTime();
         LOG.debug("Running data service using local API story");
         SDK42StyleLocalApiStory localApiStory = new SDK42StyleLocalApiStory(false, false);
         localApiStory.runBare();
-        
+    }
+    
+    @Test
+    public void secureLocalSDKApiTests() throws Throwable {    
         // create and run a secure caGrid Data Service using the SDK's local API
-        splitTime();
         LOG.debug("Running secure data service using local API story");
         SDK42StyleLocalApiStory secureLocalApiStory = new SDK42StyleLocalApiStory(true, false);
         secureLocalApiStory.runBare();
-        
+    }
+    
+    @Test
+    public void remoteSDKApiTests() throws Throwable {
         // create and run a caGrid Data Service using the SDK's remote API
-        splitTime();
         LOG.debug("Running data service using remote API story");
         SDK42StyleRemoteApiStory remoteApiStory = new SDK42StyleRemoteApiStory(false);
         remoteApiStory.runBare();
-        
+    }
+    
+    @Test
+    public void secureRemoteSDKApiTests() throws Throwable {
         // create and run a secure caGrid Data Service using the SDK's remote API
-        splitTime();
         LOG.debug("Running secure data service using remote API story");
         SDK42StyleRemoteApiStory secureRemoteApiStory = new SDK42StyleRemoteApiStory(true);
         secureRemoteApiStory.runBare();
-        
-        splitTime();
+    }
+    
+    @Test
+    public void localSDKApiWithCsmTests() throws Throwable {
         LOG.debug("Running caCORE SDK example project with CSM creation story");
         CreateExampleProjectStory createExampleWithCsmStory = new CreateExampleProjectStory(tempApplicationDir, true);
         createExampleWithCsmStory.runBare();
 
         // create and run a caGrid Data Service using the SDK's local API
-        splitTime();
         LOG.debug("Running secure data service using local API and CSM story");
         SDK42StyleLocalApiStory localApiWithCsmStory = new SDK42StyleLocalApiStory(true, true);
         localApiWithCsmStory.runBare();
     }
     
     
-    @After
-    public void cleanUp() {
+    @AfterClass
+    public static void cleanUp() {
         LOG.debug("Cleaning up after tests");
         // throw away the temp sdk dir
         LOG.debug("Deleting temp application base dir: " + tempApplicationDir.getAbsolutePath());
         Utils.deleteDir(tempApplicationDir);
-    }
-    
-    
-    private void splitTime() {
-        if (lastTime == 0) {
-            System.out.println("Timer started");
-        } else {
-            System.out.println("Time elapsed: " 
-                + (System.currentTimeMillis() - lastTime) / 1000D + " sec");
-        }
-        lastTime = System.currentTimeMillis();
-    }
-    
+    }  
 
     public static void main(String[] args) {
         TestRunner runner = new TestRunner();
