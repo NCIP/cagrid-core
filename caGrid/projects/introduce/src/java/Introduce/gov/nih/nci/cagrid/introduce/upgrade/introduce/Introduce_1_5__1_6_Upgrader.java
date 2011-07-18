2069ddbfea06ffba938b09d18c9fa0fbe2f2f303
jgeorge <jgeorge>
2011-07-18 15:11:17 +0000

    Updated the caGrid version (and references) from 1.4-dev to 1.6-dev
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@18059 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/upgrade/introduce/Introduce_1_5__1_6_Upgrader.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.cagrid.introduce.upgrade.introduce;
+
+import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
+import gov.nih.nci.cagrid.introduce.upgrade.common.IntroduceUpgradeStatus;
+import gov.nih.nci.cagrid.introduce.upgrade.one.x.IntroduceUpgraderBase;
+
+
+public class Introduce_1_5__1_6_Upgrader extends IntroduceUpgraderBase {
+
+    public Introduce_1_5__1_6_Upgrader(IntroduceUpgradeStatus status, ServiceInformation serviceInformation,
+        String servicePath) throws Exception {
+        super(status, serviceInformation, servicePath, "1.5", "1.6");
+    }
+
+    protected void upgrade() throws Exception {
+
+    }
+    
+    protected void fixDevBuildDeploy() throws Exception{
+
+    }
+
+
+    protected void fixSecurityOnMetadataAccessProviders() {
+
+    }
+
+
+    protected void fixSource() throws Exception {
+ 
+    }
+
+
+    protected void fixWSDD() throws Exception {
+
+    }
+
+
+}
