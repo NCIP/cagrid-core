e9ec3a84159ec35476007a809341443d297a4f26
jgeorge <jgeorge>
2011-07-20 14:09:59 +0000

    [CAGRID-749] Upgraded Dorian with Introduce 1.5
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_5_release/Software/core@18079 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/dorian/src/org/cagrid/gaards/dorian/service/globus/DorianAuthorization.java
+++ caGrid/projects/dorian/src/org/cagrid/gaards/dorian/service/globus/DorianAuthorization.java
@@ -28,15 +28,10 @@ import org.w3c.dom.Node;
  * authorization requirements placed on each method call.  Each method will either return
  * upon a successful authorization or will throw an exception upon a failed authorization.
  * 
- * @created by Introduce Toolkit version 1.4
+ * @created by Introduce Toolkit version 1.5
  * 
  */
 public class DorianAuthorization implements PDP {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	public static final String SERVICE_NAMESPACE = "http://cagrid.nci.nih.gov/Dorian";
 	
