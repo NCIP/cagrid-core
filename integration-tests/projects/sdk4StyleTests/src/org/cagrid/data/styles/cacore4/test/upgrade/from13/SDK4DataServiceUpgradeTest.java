e84c9d1fa8bc0d136c70e8676c76b1114d3307f3
ervind <ervind>
2011-08-03 15:55:00 +0000

    CAGRID-750: upgrade tests for 1.3 to 1.5
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_5_release/Software/core@18118 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ integration-tests/projects/sdk4StyleTests/src/org/cagrid/data/styles/cacore4/test/upgrade/from13/SDK4DataServiceUpgradeTest.java
@@ -0,0 +1,48 @@
+package org.cagrid.data.styles.cacore4.test.upgrade.from13;
+
+import org.cagrid.data.styles.cacore4.test.upgrade.UpgradeStory;
+import org.cagrid.data.test.creation.DataTestCaseInfo;
+
+
+public class SDK4DataServiceUpgradeTest extends UpgradeStory {
+
+    public SDK4DataServiceUpgradeTest() {
+        super();
+    }
+
+
+    public String getDescription() {
+        return "Tests upgrading a caGrid 1.3 Data Service backed by caCORE SDK 4.0 to the current version";
+    }
+
+
+    public DataTestCaseInfo getTestCaseInfo() {
+        DataTestCaseInfo info = new DataTestCaseInfo() {
+            public String getPackageName() {
+                return "org.cagrid.test.data.with.sdk40";
+            }
+            
+            
+            public String getNamespace() {
+                return "http://sdk40.with.data.test.cagrid.org/DataServiceWithSdk40";
+            }
+            
+            
+            public String getName() {
+                return "DataServiceWithSdk40";
+            }
+            
+            
+            protected String getServiceDirName() {
+                return "DataServiceWithSdk40";
+            }
+        };
+        return info;
+    }
+
+
+    @Override
+    public String getServiceZipName() {
+        return "DataServiceWithSdk40-1.3.zip";
+    }
+}
