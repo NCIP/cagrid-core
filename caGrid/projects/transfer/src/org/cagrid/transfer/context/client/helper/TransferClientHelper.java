593f0eca96148ac435c9c5a2a98ce91a4cff195f
ervind <ervind>
2011-09-14 18:27:00 +0000

    cleanup to make this easier to debug
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@18209 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/transfer/src/org/cagrid/transfer/context/client/helper/TransferClientHelper.java
+++ caGrid/projects/transfer/src/org/cagrid/transfer/context/client/helper/TransferClientHelper.java
@@ -20,34 +20,40 @@ public class TransferClientHelper {
     /**
      * Returns a handle to the input stream of the socket which is returning the
      * data referred to by the descriptor. This method can make an https
-     * connection if desired using the credentials passed in.  If you wish to use this
-     * method to connect to http it will not use the Crediential whether you pass 
-     * them in or they are null,
+     * connection if desired using the credentials passed in. If you wish to use
+     * this method to connect to http it will not use the Crediential whether
+     * you pass them in or they are null,
      * 
-     * @param desc              data transfer descriptor received from TransferServiceContextClient
-     * @param creds             creator of the transfer resource credentials
+     * @param desc
+     *            data transfer descriptor received from
+     *            TransferServiceContextClient
+     * @param creds
+     *            creator of the transfer resource credentials
      * @return
      * @throws Exception
      */
     public static InputStream getData(DataTransferDescriptor desc, GlobusCredential creds) throws Exception {
+        InputStream dataStream = null;
         URL url = new URL(desc.getUrl());
         if (url.getProtocol().equals("http")) {
             URLConnection conn = url.openConnection();
             conn.connect();
-            return conn.getInputStream();
+            dataStream =  conn.getInputStream();
         } else if (url.getProtocol().equals("https")) {
-            if(creds!=null){
-            GlobusGSSCredentialImpl cred = new GlobusGSSCredentialImpl(creds, GSSCredential.INITIATE_AND_ACCEPT);
-            GSIHttpURLConnection connection = new GSIHttpURLConnection(url);
-            connection.setGSSMode(GSIConstants.MODE_SSL);
-            connection.setCredentials(cred);
-            return connection.getInputStream();
+            if (creds != null) {
+                GlobusGSSCredentialImpl cred = new GlobusGSSCredentialImpl(creds, GSSCredential.INITIATE_AND_ACCEPT);
+                GSIHttpURLConnection connection = new GSIHttpURLConnection(url);
+                connection.setGSSMode(GSIConstants.MODE_SSL);
+                connection.setCredentials(cred);
+                dataStream = connection.getInputStream();
             } else {
                 throw new Exception(
-                "To use the https protocol to retrieve data from the Transfer Service you must have credentials");
+                    "To use the https protocol to retrieve data from the Transfer Service you must have credentials");
             }
+        } else {
+            throw new Exception("Protocol " + url.getProtocol() + " not supported.");
         }
-        throw new Exception("Protocol " + url.getProtocol() + " not supported.");
+        return dataStream;
     }
 
 
@@ -56,7 +62,9 @@ public class TransferClientHelper {
      * data referred to by the descriptor. This method cannot make secure https
      * connects and only works with http.
      * 
-     * @param desc              data transfer descriptor received from TransferServiceContextClient
+     * @param desc
+     *            data transfer descriptor received from
+     *            TransferServiceContextClient
      * @return
      * @throws Exception
      */
@@ -66,18 +74,25 @@ public class TransferClientHelper {
 
 
     /**
-     * Reads from the input stream to put the data to the server. Be sure to close the stream when
-     * done writing the data. This method can use http and https if the
-     * credentials are provided.  This is a blocking call. Will return with the entire data has been transmitted.
+     * Reads from the input stream to put the data to the server. Be sure to
+     * close the stream when done writing the data. This method can use http and
+     * https if the credentials are provided. This is a blocking call. Will
+     * return with the entire data has been transmitted.
      * 
-     * @param is                input stream providing the data
-     * @param contentLength     number of bytes in the input stream to be read
-     * @param desc              data transfer descriptor received from TransferServiceContextClient
-     * @param creds             creator of the transfer resource credentials
+     * @param is
+     *            input stream providing the data
+     * @param contentLength
+     *            number of bytes in the input stream to be read
+     * @param desc
+     *            data transfer descriptor received from
+     *            TransferServiceContextClient
+     * @param creds
+     *            creator of the transfer resource credentials
      * @return
      * @throws Exception
      */
-    public static void putData(InputStream is, long contentLength, DataTransferDescriptor desc, GlobusCredential creds) throws Exception {
+    public static void putData(InputStream is, long contentLength, DataTransferDescriptor desc, GlobusCredential creds)
+        throws Exception {
         URL url = new URL(desc.getUrl());
         if (url.getProtocol().equals("http")) {
             PostMethod post = new PostMethod(desc.getUrl());
@@ -85,48 +100,49 @@ public class TransferClientHelper {
             post.setRequestEntity(re);
             HttpClient client = new HttpClient();
             int status = client.executeMethod(post);
-            return;
         } else if (url.getProtocol().equals("https")) {
-            if(creds!=null){
-            GlobusGSSCredentialImpl cred = new GlobusGSSCredentialImpl(creds, GSSCredential.INITIATE_AND_ACCEPT);
-            GSIHttpURLConnection connection = new GSIHttpURLConnection(url);
-            connection.setGSSMode(GSIConstants.MODE_SSL);
-            connection.setCredentials(cred);
-            try {
-                int l;
-                byte[] buffer = new byte[1024];
-                while ((l = is.read(buffer)) != -1) {
-                    connection.getOutputStream().write(buffer, 0, l);
+            if (creds != null) {
+                GlobusGSSCredentialImpl cred = new GlobusGSSCredentialImpl(creds, GSSCredential.INITIATE_AND_ACCEPT);
+                GSIHttpURLConnection connection = new GSIHttpURLConnection(url);
+                connection.setGSSMode(GSIConstants.MODE_SSL);
+                connection.setCredentials(cred);
+                try {
+                    int l = -1;
+                    byte[] buffer = new byte[1024];
+                    while ((l = is.read(buffer)) != -1) {
+                        connection.getOutputStream().write(buffer, 0, l);
+                    }
+                } finally {
+                    is.close();
                 }
-            } finally {
-                is.close();
-            }
-            connection.getOutputStream().close();
-            connection.getInputStream().close();
-            return;
+                connection.getOutputStream().close();
+                connection.getInputStream().close();
             } else {
                 throw new Exception(
-                "To use the https protocol to stage data to the Transfer Service you must have credentials");
+                    "To use the https protocol to stage data to the Transfer Service you must have credentials");
             }
+        } else {
+            throw new Exception("Protocol " + url.getProtocol() + " not supported.");
         }
-        throw new Exception("Protocol " + url.getProtocol() + " not supported.");
     }
 
 
     /**
-     * Reads from the input stream to put the data to the server. This method can only put to an
-     * http connection and not an https one. Be sure to close the stream when
-     * done writing to it. Will return with the entire data has been transmitted.
+     * Reads from the input stream to put the data to the server. This method
+     * can only put to an http connection and not an https one. Be sure to close
+     * the stream when done writing to it. Will return with the entire data has
+     * been transmitted.
      * 
-     * @param is                input stream providing the data
-     * @param contentLength     number of bytes in the input stream to be read
-     * @param desc              data transfer descriptor received from TransferServiceContextClient              
-     * @param creds             creator of the transfer resource credentials
-     * @return
+     * @param is
+     *            input stream providing the data
+     * @param contentLength
+     *            number of bytes in the input stream to be read
+     * @param desc
+     *            data transfer descriptor received from
+     *            TransferServiceContextClient
      * @throws Exception
      */
     public static void putData(InputStream is, long contentLength, DataTransferDescriptor desc) throws Exception {
         putData(is, contentLength, desc, null);
     }
-
 }
