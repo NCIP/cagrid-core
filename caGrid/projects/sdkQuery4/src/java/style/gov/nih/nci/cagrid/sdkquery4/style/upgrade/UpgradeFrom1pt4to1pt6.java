88be0dfdd1338634eca18cb1c600c406f8909871
ervind <ervind>
2011-09-30 19:13:19 +0000

    CAGRID-792: Forgot to update the jars
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@18289 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/upgrade/UpgradeFrom1pt4to1pt6.java
+++ caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/upgrade/UpgradeFrom1pt4to1pt6.java
@@ -1,19 +1,25 @@
 package gov.nih.nci.cagrid.sdkquery4.style.upgrade;
 
 import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.data.extension.Data;
 import gov.nih.nci.cagrid.data.style.ServiceStyleContainer;
 import gov.nih.nci.cagrid.data.style.ServiceStyleLoader;
 import gov.nih.nci.cagrid.data.style.StyleVersionUpgrader;
 import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionTypeExtensionData;
 import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
+import gov.nih.nci.cagrid.introduce.extension.utils.AxisJdomUtils;
 import gov.nih.nci.cagrid.introduce.upgrade.common.ExtensionUpgradeStatus;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.axis.message.MessageElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jdom.Element;
+import org.jdom.JDOMException;
 
 public class UpgradeFrom1pt4to1pt6 implements StyleVersionUpgrader {
     
@@ -58,5 +64,67 @@ public class UpgradeFrom1pt4to1pt6 implements StyleVersionUpgrader {
             LOG.debug(message);
             status.addDescriptionLine(message);
         }
+        
+        // edit data service extension data's "additional jars" to point to the ones we added instead of the old ones
+        Element data = getExtensionDataElement(extensionData);
+        Element libsElement = data.getChild("AdditionalLibraries", data.getNamespace());
+        if (libsElement != null) {
+            List<?> jarNameElements = libsElement.getChildren("JarName", libsElement.getNamespace());
+            if (jarNameElements != null && jarNameElements.size() != 0) {
+                Iterator<?> jarNameElemIter = jarNameElements.iterator();
+                while (jarNameElemIter.hasNext()) {
+                    Element jarNameElement = (Element) jarNameElemIter.next();
+                    if (removedLibs.contains(jarNameElement.getText())) {
+                        jarNameElemIter.remove();
+                    }
+                }
+            }
+            for (String added : addedLibs) {
+                Element jarNameElement = new Element("JarName", libsElement.getNamespace());
+                jarNameElement.setText(added);
+                libsElement.addContent(jarNameElement);
+            }
+        }
+        storeExtensionDataElement(extensionData, data);
+        status.addDescriptionLine("Reconfigured searchable jars for query processor discovery panel");
+    }
+    
+    
+    private Element getExtensionDataElement(ExtensionTypeExtensionData extensionData) throws Exception {
+        MessageElement[] anys = extensionData.get_any();
+        MessageElement rawDataElement = null;
+        for (int i = 0; (anys != null) && (i < anys.length); i++) {
+            if (anys[i].getQName().equals(Data.getTypeDesc().getXmlType())) {
+                rawDataElement = anys[i];
+                break;
+            }
+        }
+        if (rawDataElement == null) {
+            throw new Exception("No extension data was found for the data service extension");
+        }
+        Element extensionDataElement = AxisJdomUtils.fromMessageElement(rawDataElement);
+        return extensionDataElement;
+    }
+    
+    
+    private void storeExtensionDataElement(ExtensionTypeExtensionData extensionData, Element elem) throws Exception {
+        MessageElement[] anys = extensionData.get_any();
+        for (int i = 0; (anys != null) && (i < anys.length); i++) {
+            if (anys[i].getQName().equals(Data.getTypeDesc().getXmlType())) {
+                // remove the old extension data
+                anys = (MessageElement[]) Utils.removeFromArray(anys, anys[i]);
+                break;
+            }
+        }
+        // create a message element from the JDom element
+        MessageElement data = null;
+        try {
+            data = AxisJdomUtils.fromElement(elem);
+        } catch (JDOMException ex) {
+            throw new Exception(
+                "Error converting extension data to Axis message element: " + ex.getMessage(), ex);
+        }
+        anys = (MessageElement[]) Utils.appendToArray(anys, data);
+        extensionData.set_any(anys);
     }
 }
