eb4c39f5038ab827bbf4195d51dffff4a02d9bf3
ervind <ervind>
2011-09-30 19:14:05 +0000

    CAGRID-791: upgraders for 1.4 and 1.5 to 1.6.  Removed upgraders for 1.2 and 1.3
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/core@18290 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- caGrid/projects/sdkQuery41/src/java/style/org/cagrid/data/sdkquery41/style/upgrade/UpgradeFrom1pt5to1pt6.java
+++ caGrid/projects/sdkQuery41/src/java/style/org/cagrid/data/sdkquery41/style/upgrade/UpgradeFrom1pt5to1pt6.java
@@ -1,13 +1,11 @@
 package org.cagrid.data.sdkquery41.style.upgrade;
 
 import gov.nih.nci.cagrid.common.Utils;
-import gov.nih.nci.cagrid.data.QueryProcessorConstants;
 import gov.nih.nci.cagrid.data.extension.Data;
 import gov.nih.nci.cagrid.data.style.ServiceStyleContainer;
 import gov.nih.nci.cagrid.data.style.ServiceStyleLoader;
 import gov.nih.nci.cagrid.data.style.StyleVersionUpgrader;
 import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionTypeExtensionData;
-import gov.nih.nci.cagrid.introduce.common.CommonTools;
 import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.extension.utils.AxisJdomUtils;
 import gov.nih.nci.cagrid.introduce.upgrade.common.ExtensionUpgradeStatus;
@@ -16,20 +14,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
-import java.util.Set;
 
 import org.apache.axis.message.MessageElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.cagrid.data.sdkquery41.processor.SDK41QueryProcessor;
-import org.cagrid.data.sdkquery41.processor2.SDK41CQL2QueryProcessor;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 
-public class UpgradeFrom1pt3to1pt4 implements StyleVersionUpgrader {
+public class UpgradeFrom1pt5to1pt6 implements StyleVersionUpgrader {
     
-    private static Log LOG = LogFactory.getLog(UpgradeFrom1pt3to1pt4.class);
+    private static Log LOG = LogFactory.getLog(UpgradeFrom1pt5to1pt6.class);
 
     public void upgradeStyle(ServiceInformation serviceInformation, ExtensionTypeExtensionData extensionData,
         ExtensionUpgradeStatus status, String serviceFromVersion, String serviceToVersion) throws Exception {
@@ -55,23 +49,12 @@ public class UpgradeFrom1pt3to1pt4 implements StyleVersionUpgrader {
             if (upgradeLib.getName().startsWith("caGrid-")) {
                 int versionIndex = upgradeLib.getName().indexOf(StyleUpgradeConstants.LATEST_JAR_SUFFIX);
                 File oldCagridMatch = new File(serviceLibDir, 
-                    upgradeLib.getName().substring(0, versionIndex) + "-1.3.jar");
-                LOG.debug("Looking for old caGrid 1.3 library " + oldCagridMatch.getName());
+                    upgradeLib.getName().substring(0, versionIndex) + "-1.5.jar");
+                LOG.debug("Looking for old caGrid 1.5 library " + oldCagridMatch.getName());
                 if (oldCagridMatch.exists()) {
                     oldCagridMatch.delete();
                     removedLibs.add(oldCagridMatch.getName());
                     LOG.debug("Deleted old library: " + oldCagridMatch.getName());
-                }
-                // since this upgrader could be called for upgrading 1.2 as well, check those jars
-                oldCagridMatch = new File(serviceLibDir, 
-                    upgradeLib.getName().substring(0, versionIndex) + "-1.2.jar");
-                LOG.debug("Looking for old caGrid 1.2 library " + oldCagridMatch.getName());
-                if (oldCagridMatch.exists()) {
-                    oldCagridMatch.delete();
-                    removedLibs.add(oldCagridMatch.getName());
-                    String message = "Deleted old library: " + oldCagridMatch.getName();
-                    LOG.debug(message);
-                    status.addDescriptionLine(message);
                 }
             }
             File copyLib = new File(serviceLibDir, upgradeLib.getName());
@@ -80,56 +63,6 @@ public class UpgradeFrom1pt3to1pt4 implements StyleVersionUpgrader {
             String message = "Copied new library: " + upgradeLib.getName();
             LOG.debug(message);
             status.addDescriptionLine(message);
-        }
-        
-        // set CQL 2 query processor classname property
-        CommonTools.setServiceProperty(serviceInformation.getServiceDescriptor(),
-            QueryProcessorConstants.CQL2_QUERY_PROCESSOR_CLASS_PROPERTY, 
-            SDK41CQL2QueryProcessor.class.getName(), false);
-        status.addDescriptionLine("Set CQL 2 query processor class service property to " 
-            + SDK41CQL2QueryProcessor.class.getName());
-        
-        // add CQL 2 query processor properties
-        SDK41CQL2QueryProcessor processor = new SDK41CQL2QueryProcessor();
-        Properties processorProperties = processor.getRequiredParameters();
-        Set<String> fromEtc = processor.getParametersFromEtc();
-        for (Object key : processorProperties.keySet()) {
-            String propName = (String) key;
-            String def = processorProperties.getProperty(propName);
-            CommonTools.setServiceProperty(serviceInformation.getServiceDescriptor(),
-                QueryProcessorConstants.CQL2_QUERY_PROCESSOR_CONFIG_PREFIX + propName,
-                def, fromEtc.contains(propName));
-        }
-        
-        // copy values from CQL 1 query processor properties
-        SDK41QueryProcessor cql1processor = new SDK41QueryProcessor();
-        Properties oldProperties = cql1processor.getRequiredParameters();
-        for (Object key : oldProperties.keySet()) {
-            String propName = (String) key;
-            String oldPrefixedName = QueryProcessorConstants.QUERY_PROCESSOR_CONFIG_PREFIX + propName;
-            String newPrefixedName = QueryProcessorConstants.CQL2_QUERY_PROCESSOR_CONFIG_PREFIX + propName;
-            if (processorProperties.containsKey(key) &&
-                CommonTools.servicePropertyExists(serviceInformation.getServiceDescriptor(), oldPrefixedName)) {
-                String copyValue = CommonTools.getServicePropertyValue(
-                    serviceInformation.getServiceDescriptor(), oldPrefixedName);
-                CommonTools.setServiceProperty(serviceInformation.getServiceDescriptor(), 
-                    newPrefixedName, copyValue, fromEtc.contains(propName));
-            }            
-        }
-        status.addDescriptionLine("Copied configuration values for CQL 2 query processor " +
-        		"from existing CQL 1 query processor configuration");
-        
-        // check for the ORM jar in the service's lib dir
-        String applicationName = CommonTools.getServicePropertyValue(
-            serviceInformation.getServiceDescriptor(),
-            QueryProcessorConstants.QUERY_PROCESSOR_CONFIG_PREFIX + SDK41QueryProcessor.PROPERTY_APPLICATION_NAME);
-        File ormJar = new File(serviceInformation.getBaseDirectory(), "lib" + File.separator + applicationName + "-orm.jar");
-        if (!ormJar.exists()) {
-            // the ORM jar contains the required hibernate config files for the CQL 2 HQL translator
-            status.addIssue("The caCORE SDK Application ORM jar was not found at " + ormJar.getAbsolutePath() + ". " 
-                + "This jar contains the required Hibernate configuration information for the new CQL 2 query processor",
-                "Copy the file " + ormJar.getName() + " from your caCORE SDK local-client/lib directory into your " 
-                + "data service's lib directory (" + serviceInformation.getBaseDirectory().getAbsolutePath() + File.separator + "lib)");
         }
         
         // edit data service extension data's "additional jars" to point to the ones we added instead of the old ones
@@ -154,8 +87,6 @@ public class UpgradeFrom1pt3to1pt4 implements StyleVersionUpgrader {
         }
         storeExtensionDataElement(extensionData, data);
         status.addDescriptionLine("Reconfigured searchable jars for query processor discovery panel");
-        status.addIssue("A CQL 2 query processor has been added to this grid data service",
-            "You do not need to supply a custom CQL 2 query processor");
     }
     
     
