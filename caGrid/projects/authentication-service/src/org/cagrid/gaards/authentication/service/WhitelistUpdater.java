ea8db4922941a4091db5b1c517e1de5e9a63a837
ervind <ervind>
2011-11-08 14:57:18 +0000

    CAGRID-800: bringing over the whitelist updater
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_4_release/Software/core@18351 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/authentication-service/src/org/cagrid/gaards/authentication/service/WhitelistUpdater.java
@@ -0,0 +1,170 @@
+package org.cagrid.gaards.authentication.service;
+
+import gov.nih.nci.security.authentication.BetterLockoutManager;
+import gov.nih.nci.security.authentication.LockoutManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.vfs2.FileChangeEvent;
+import org.apache.commons.vfs2.FileListener;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.VFS;
+import org.apache.commons.vfs2.impl.DefaultFileMonitor;
+
+
+/**
+ * WhitelistUpdater Tool to listen for changes to the whitelist file and update
+ * the lockout manager accordingly
+ * 
+ * @author ervin
+ */
+public class WhitelistUpdater {
+    
+    private static Log LOG = LogFactory.getLog(WhitelistUpdater.class);
+
+    private File monitoredDirectory = null;
+    private String monitoredFilename = null;
+    private Set<String> previousWhitelist = null;
+    
+    
+    public static void monitorWhitelist(String whitelistFile) throws Exception {
+        WhitelistUpdater updater = new WhitelistUpdater(whitelistFile);
+        updater.startMonitoring();
+    }
+    
+
+    private WhitelistUpdater(String whitelistFilename) {
+        File file = new File(whitelistFilename);
+        this.monitoredDirectory = file.getParentFile();
+        this.monitoredFilename = file.getName();
+        LOG.debug("Monitoring for changes to " + monitoredFilename + " in directory " + monitoredDirectory);
+        this.previousWhitelist = new HashSet<String>();
+    }
+
+
+    public void startMonitoring() throws Exception {
+        FileSystemManager fsManager = VFS.getManager();
+        FileObject listendir = fsManager.resolveFile(monitoredDirectory.getAbsolutePath());
+        FileListener listener = new FileListener() {
+            
+            @Override
+            public void fileDeleted(FileChangeEvent event) throws Exception {
+                // verify the filename is the one we're interested in
+                String filename = event.getFile().getName().getBaseName();
+                if (monitoredFilename.equals(filename)) {
+                    LOG.info("Whitelist file " + monitoredFilename + " was deleted.  Purging whitelist");
+                    BetterLockoutManager lockoutManager = LockoutManager.getInstance().getDelegatedLockoutManager();
+                    synchronized (previousWhitelist) {
+                        // delist all previously known whitelisted users
+                        // doing this rather than a blanket removal of every ID in the whitelist
+                        // preserves any IDs that might have been added by other mechanisms
+                        // than the whitelist file.
+                        for (String whitelistedUser : previousWhitelist) {
+                            lockoutManager.unWhitelistUser(whitelistedUser);
+                        }
+                        previousWhitelist.clear();
+                    }
+                } else {
+                    LOG.debug("Noticed the file " + filename + " was deleted; ignoring since it's not " + monitoredFilename);
+                }
+            }
+            
+            
+            @Override
+            public void fileCreated(FileChangeEvent event) throws Exception {
+                // verify the filename is the one we're interested in
+                String filename = event.getFile().getName().getBaseName();
+                if (monitoredFilename.equals(filename)) {
+                    LOG.info("Whitelist file " + monitoredFilename + " was created.  Loading whitelist");
+                    BetterLockoutManager lockoutManager = LockoutManager.getInstance().getDelegatedLockoutManager();
+                    synchronized (previousWhitelist) {
+                        try {
+                            File whitelistFile = new File(event.getFile().getName().getPathDecoded());
+                            BufferedReader reader = new BufferedReader(new FileReader(whitelistFile));
+                            String userId = null;
+                            while ((userId = reader.readLine()) != null) {
+                                userId = userId.trim();
+                                LOG.debug("Found " + userId + " in the whitelist");
+                                lockoutManager.whitelistUser(userId);
+                                previousWhitelist.add(userId);
+                            }
+                        } catch (IOException ex) {
+                            LOG.error("Error reading whitelist: " + ex.getMessage(), ex);
+                            LOG.error("Suggest deleting the whitelist file to purge, and recreating the list");
+                        }
+                    }
+                } else {
+                    LOG.debug("Noticed the file " + filename + " was created; ignoring since it's not " + monitoredFilename);
+                }
+            }
+            
+            
+            @Override
+            public void fileChanged(FileChangeEvent event) throws Exception {
+             // verify the filename is the one we're interested in
+                String filename = event.getFile().getName().getBaseName();
+                if (monitoredFilename.equals(filename)) {
+                    LOG.info("Whitelist file " + monitoredFilename + " was changed.  Loading whitelist");
+                    BetterLockoutManager lockoutManager = LockoutManager.getInstance().getDelegatedLockoutManager();
+                    synchronized (previousWhitelist) {
+                        try {
+                            File whitelistFile = new File(event.getFile().getName().getPathDecoded());
+                            BufferedReader reader = new BufferedReader(new FileReader(whitelistFile));
+                            Set<String> loadedWhitelist = new HashSet<String>();
+                            String userId = null;
+                            while ((userId = reader.readLine()) != null) {
+                                userId = userId.trim();
+                                LOG.debug("Found " + userId + " in the whitelist");
+                                lockoutManager.whitelistUser(userId);
+                                loadedWhitelist.add(userId);
+                            }
+                            reader.close();
+                            // remove any entries from the previous whitelist that are NOT in the loaded one
+                            previousWhitelist.removeAll(loadedWhitelist);
+                            for (String removedId : previousWhitelist) {
+                                LOG.debug("User " + removedId + " was not in the new list; removing");
+                                lockoutManager.unWhitelistUser(removedId);
+                            }
+                            previousWhitelist.clear();
+                            previousWhitelist.addAll(loadedWhitelist);
+                        } catch (IOException ex) {
+                            LOG.error("Error reading whitelist: " + ex.getMessage(), ex);
+                            LOG.error("Suggest deleting the whitelist file to purge, and recreating the list");
+                        }
+                    }
+                } else {
+                    LOG.debug("Noticed the file " + filename + " was changed; ignoring since it's not " + monitoredFilename);
+                }
+            }
+        };
+        DefaultFileMonitor fm = new DefaultFileMonitor(listener);
+        fm.setRecursive(true);
+        fm.addFile(listendir);
+        fm.start();
+        // if the file already exists, fire off a fake event to the listener to force-load the list
+        FileObject whitelist = fsManager.resolveFile(monitoredDirectory, monitoredFilename);
+        if (whitelist.exists()) {
+            listener.fileCreated(new FileChangeEvent(whitelist));
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        try {
+            WhitelistUpdater.monitorWhitelist("etc/lockout-whitelist.txt");
+            String waitForIt = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            System.out.println(waitForIt);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ caGrid/projects/authentication-service/test/src/org/cagrid/gaards/authentication/service/TestWhitelistUpdater.java
@@ -0,0 +1,159 @@
+package org.cagrid.gaards.authentication.service;
+
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.security.authentication.LockoutManager;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+import junit.framework.TestCase;
+
+public class TestWhitelistUpdater extends TestCase {
+    
+    public static long LOCKOUT_DURATION = 10000;
+    public static int MAX_ATTEMPTS = 4;
+    public static long LOCKOUT_MEMORY = 500;
+    
+    private File whitelistFile = null;
+    private Random rand = null;
+    
+    public TestWhitelistUpdater() {
+        LockoutManager.initialize(String.valueOf(LOCKOUT_DURATION), String.valueOf(LOCKOUT_MEMORY), String.valueOf(MAX_ATTEMPTS));
+    }
+    
+    
+    public void setUp() {
+        try {
+            File tmpDir = new File("./tmp");
+            tmpDir.mkdir();
+            this.whitelistFile = File.createTempFile("TempWhitelist", ".txt", tmpDir);
+            this.rand = new Random();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            fail("Error creating the temporary whitelist file: " + ex.getMessage());
+        }
+        try {
+            // running the whitelist updater in a thread because it needs to recieve asynchronous updates
+            // when the file system changes
+            Runnable runme = new Runnable() {
+                public void run() {
+                    try {
+                        WhitelistUpdater.monitorWhitelist(whitelistFile.getAbsolutePath());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(runme).start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error setting up the whitelist monitor: " + ex.getMessage());
+        }
+    }
+    
+    
+    public void tearDown() {
+        if (this.whitelistFile.exists()) {
+            this.whitelistFile.delete();
+        }
+    }
+    
+    
+    private String getFakeUserName() {
+        return "Test_User_" + rand.nextLong();
+    }
+    
+    
+    private void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    private void addIdToWhitelist(String userId) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(this.whitelistFile));
+            writer.write(userId);
+            writer.write("\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            fail("Error writing to whitelist file: " + ex.getMessage());
+        }
+    }
+    
+    
+    private void removeIdFromWhitelist(String userId) {
+        try {
+            StringBuffer buff = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new FileReader(this.whitelistFile));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!userId.equals(line)) {
+                    buff.append(line).append("\n");
+                }
+            }
+            reader.close();
+            this.whitelistFile.delete();
+            Utils.stringBufferToFile(buff, this.whitelistFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error writing to whitelist file: " + ex.getMessage());
+        }
+    }
+    
+    
+    public void testAccountWhitelisted() {
+        String userId = getFakeUserName();
+        addIdToWhitelist(userId);
+        sleep(LOCKOUT_DURATION / 2);
+        // give that a moment to be picked up
+        for (int i = 0; i < MAX_ATTEMPTS * 2; i++) {
+            LockoutManager.getInstance().setFailedAttempt(userId);
+        }
+        assertFalse("User was locked out, even though user was whitelisted", LockoutManager.getInstance().isUserLockedOut(userId));
+    }
+    
+    
+    public void testLockoutThenWhitelist() {
+        String userId = getFakeUserName();
+        for (int i = 0; i < MAX_ATTEMPTS * 2; i++) {
+            LockoutManager.getInstance().setFailedAttempt(userId);
+        }
+        assertTrue("User was not locked out", LockoutManager.getInstance().isUserLockedOut(userId));
+        addIdToWhitelist(userId);
+        // give that a moment to be seen by the whitelist updater
+        sleep(LOCKOUT_DURATION / 2);
+        for (int i = 0; i < MAX_ATTEMPTS * 2; i++) {
+            LockoutManager.getInstance().setFailedAttempt(userId);
+        }
+        assertFalse("User was locked out, even though user was whitelisted", LockoutManager.getInstance().isUserLockedOut(userId));
+    }
+    
+    
+    public void testRemoveFromWhitelist() {
+        String userId = getFakeUserName();
+        addIdToWhitelist(userId);
+        sleep(LOCKOUT_DURATION / 2);
+        for (int i = 0; i < MAX_ATTEMPTS * 2; i++) {
+            LockoutManager.getInstance().setFailedAttempt(userId);
+        }
+        assertFalse("User was locked out, even though user was whitelisted", LockoutManager.getInstance().isUserLockedOut(userId));
+        removeIdFromWhitelist(userId);
+        sleep(LOCKOUT_DURATION / 2);
+        for (int i = 0; i < MAX_ATTEMPTS * 2; i++) {
+            LockoutManager.getInstance().setFailedAttempt(userId);
+        }
+        assertTrue("User was not locked out", LockoutManager.getInstance().isUserLockedOut(userId));
+    }
+}
