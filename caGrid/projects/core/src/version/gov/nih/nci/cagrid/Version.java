4f2cf7f04ed005dd77e86add524693e600e7dc96
ervind <ervind>
2012-02-17 21:07:07 +0000

    nifty version stuff
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/caGrid-1_5_release/Software/core@18576 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ caGrid/projects/core/src/version/gov/nih/nci/cagrid/Version.java
@@ -0,0 +1,63 @@
+package gov.nih.nci.cagrid;
+
+public class Version {
+
+    /**
+     * Gets the version of caGrid
+     * for caGrid 1.2.3.4, this returns 1
+     * 
+     * @return
+     */
+    public static final int getStreamVersion() {
+        return 1;
+    }
+    
+    
+    /**
+     * Gets the major release version number of caGrid
+     * for caGrid 1.2.3.4, this returns 2
+     * 
+     * @return
+     */
+    public static final int getMajorReleaseVersion() {
+        return 6;
+    }
+    
+    
+    /**
+     * Gets the minor release version number of caGrid
+     * for caGrid 1.2.3.4, this returns 3
+     * 
+     * @return
+     */
+    public static final int getMinorReleaseVersion() {
+        return 0;
+    }
+    
+    
+    /**
+     * Gets the point release version number of caGrid
+     * for caGrid 1.2.3.4, this returns 4
+     * 
+     * @return
+     */
+    public static final int getPointReleaseVersion() {
+        return 0;
+    }
+    
+    
+    public static int compareTo(String versionString) {
+        return getVersionString().compareTo(versionString);
+    }
+    
+    
+    public static String getVersionString() {
+        return "" + getStreamVersion() + "." + getMajorReleaseVersion() + "." 
+            + getMinorReleaseVersion() + "." + getPointReleaseVersion();
+    }
+    
+    
+    public static void main(String[] args) {
+        System.out.println("The caGrid Version is " + getVersionString());
+    }
+}
